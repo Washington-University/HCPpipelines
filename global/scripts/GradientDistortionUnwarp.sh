e4f7cc213d0cd4eec8b508f1242db221ab081357
Tony Wilson <wilsont@mir.wustl.edu>
2012-05-13 15:09:34 -0500

    PreFreeSurfer batch and pipeline running

--- global/scripts/GradientDistortionUnwarp.sh
+++ global/scripts/GradientDistortionUnwarp.sh
@@ -1,15 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash
 
 WorkingDirectory="$1"
 InputCoefficents="$2"
 InputFile="$3"
 OutputFile="$4"
 OutputTransform="$5"
-GlobalScripts="$6"
 
 DIR=`pwd`
 cd $WorkingDirectory
 fslroi "$InputFile".nii.gz "$InputFile"_vol1.nii.gz 0 1
+echo "gradient_unwarp.py "$InputFile"_vol1.nii.gz trilinear.nii.gz siemens -g "$InputCoefficents" -n"
 gradient_unwarp.py "$InputFile"_vol1.nii.gz trilinear.nii.gz siemens -g "$InputCoefficents" -n
 convertwarp --ref=trilinear.nii.gz --premat=shiftMatrix.mat --warp1=fullWarp.nii.gz --out="$OutputTransform"
 applywarp --interp=spline -i "$InputFile" -r "$InputFile"_vol1.nii.gz -w "$OutputTransform" -o "$OutputFile"
