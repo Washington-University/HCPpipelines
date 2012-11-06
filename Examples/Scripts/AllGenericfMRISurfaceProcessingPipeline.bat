16ab3cacefbd43ef1083826bee9c02228effca42
Matt Glasser <glasserm@wusm.wustl.edu>
2012-11-06 12:18:44 -0600

    *Initial Commit of Revised Examples including Examples/Scripts, which has example batch scripts that could be run on the example subject, Examples/792564.  Some bugs are possible in this initial commit.

---
+++ Examples/Scripts/AllGenericfMRISurfaceProcessingPipeline.bat
@@ -0,0 +1,37 @@
+Subjlist="792564"
+GitRepo="/media/2TBB/Connectome_Project/Pipelines"
+StudyFolder="/media/myelin/brainmappers/Connectome_Project/TestStudyFolder" #Path to subject's data folder
+
+
+Tasklist="MOTOR1 MOTOR2"
+
+
+for Subject in $Subjlist ; do
+  for Task in $Tasklist ; do
+    if [ -z `echo $Task | grep REST` ] ; then
+      Type="tfMRI"
+    else
+      Type="rfMRI"
+    fi
+    Subject="$Subject"
+    OutputNameOffMRI="${Type}_${Task}"
+    DownSampleNameI="32"
+    FinalfMRIResolution="2"
+    SmoothingFWHM="2"
+    Caret5_Command="/usr/bin/caret_command" #Location of Caret5 caret_command
+    Caret7_Command="wb_command"
+    PipelineScripts="${GitRepo}/fMRISurface/scripts"
+    AtlasParcellation="${GitRepo}/global/templates/standard_mesh_atlases/CCNMD_MyelinMapping_Avgwmparc.nii.gz"
+    AtlasSurfaceROIs="${GitRepo}/global/templates/standard_mesh_atlases/fs_L/CCNMD_MyelinMapping.L.Atlas_Cortex_ROI.164k_fs_LR.func.gii@${GitRepo}/global/templates/standard_mesh_atlases/fs_R/CCNMD_MyelinMapping.R.Atlas_Cortex_ROI.164k_fs_LR.func.gii" #delimit left and right files with @
+    BrainOrdinatesResolution="2" #Could be the same as FinalfRMIResolution something different, which will call a different module for subcortical processing
+    SubcorticalBrainOrdinatesLabels="${GitRepo}/global/config/FreeSurferSubcorticalLabelTableLut.txt"
+    
+    if [ -e "$StudyFolder"/"$Subject"/MNINonLinear/Results/"$OutputNameOffMRI"/"$OutputNameOffMRI".nii.gz ] ; then
+      fsl_sub -q long.q ${GitRepo}/fMRISurface/GenericfMRISurfaceProcessingPipeline.sh $StudyFolder $Subject $OutputNameOffMRI $DownSampleNameI $FinalfMRIResolution $SmoothingFWHM $Caret5_Command $Caret7_Command $PipelineScripts $AtlasParcellation $AtlasSurfaceROIs $BrainOrdinatesResolution $SubcorticalBrainOrdinatesLabels
+      echo "set -- $StudyFolder $Subject $OutputNameOffMRI $DownSampleNameI $FinalfMRIResolution $SmoothingFWHM $Caret5_Command $Caret7_Command $PipelineScripts $AtlasParcellation $AtlasSurfaceROIs $BrainOrdinatesResolution $SubcorticalBrainOrdinatesLabels"
+    else
+      echo "fMRI Run ""$OutputNameOffMRI"" Not Found"
+    fi      
+  done
+done
+
---
+++ Examples/Scripts/FreeSurferPipeLine.bat
@@ -0,0 +1,21 @@
+Subjlist="792564"
+GitRepo="/media/2TBB/Connectome_Project/Pipelines"
+StudyFolder="/media/myelin/brainmappers/Connectome_Project/TestStudyFolder" #Path to subject's data folder
+
+
+for Subject in $Subjlist ; do
+  #Input Variables
+  SubjectID="$Subject" #FreeSurfer Subject ID Name
+  SubjectDIR="${StudyFolder}/${Subject}/T1w" #Location to Put FreeSurfer Subject's Folder
+  T1wImage="${StudyFolder}/${Subject}/T1w/T1w_acpc_dc_restore.nii.gz" #T1w FreeSurfer Input (Full Resolution)
+  T1wImageBrain="${StudyFolder}/${Subject}/T1w/T1w_acpc_dc_restore_brain.nii.gz" #T1w FreeSurfer Input (Full Resolution)
+  T2wImage="${StudyFolder}/${Subject}/T1w/T2w_acpc_dc_restore.nii.gz" #T2w FreeSurfer Input (Full Resolution)
+  PipelineScripts="${GitRepo}/FreeSurfer/scripts"
+  PipelineBinaries="${GitRepo}/global/binaries"
+  Caret5_Command="/usr/bin/caret_command"
+  Caret7_Command="wb_command"
+
+  fsl_sub -q short.q ${GitRepo}/FreeSurfer/FreeSurferPipeline.sh "$SubjectID" "$SubjectDIR" "$T1wImage" "$T1wImageBrain" "$T2wImage" "$PipelineScripts" "$PipelineBinaries" "$Caret5_Command" "$Caret7_Command"
+  echo "set -- $SubjectID $SubjectDIR $T1wImage $T1wImageBrain $T2wImage $PipelineScripts $PipelineBinaries $Caret5_Command $Caret7_Command"
+done
+
