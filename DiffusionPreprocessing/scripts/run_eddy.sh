e619ec8053943bd8d460c0349913a6484fd1d297
Stam Sotiropoulos <stam@fmrib.ox.ac.uk>
2013-01-24 17:21:40 +0000

    changed fwhm for eddy's new binary

--- DiffusionPreprocessing/scripts/run_eddy.sh
+++ DiffusionPreprocessing/scripts/run_eddy.sh
@@ -8,7 +8,7 @@ topupdir=`dirname ${workingdir}`/topup
 
 ${FSLDIR}/bin/imcp ${topupdir}/nodif_brain_mask ${workingdir}/
 
-${binarydir}/eddy --imain=${workingdir}/Pos_Neg --mask=${workingdir}/nodif_brain_mask --index=${workingdir}/index.txt --acqp=${workingdir}/acqparams.txt --bvecs=${workingdir}/Pos_Neg.bvecs --bvals=${workingdir}/Pos_Neg.bvals --fwhm=5 --topup=${topupdir}/topup_Pos_Neg_b0 --out=${workingdir}/eddy_unwarped_images --flm=quadratic -v #--resamp=lsr #--session=${workingdir}/series_index.txt
+${binarydir}/eddy --imain=${workingdir}/Pos_Neg --mask=${workingdir}/nodif_brain_mask --index=${workingdir}/index.txt --acqp=${workingdir}/acqparams.txt --bvecs=${workingdir}/Pos_Neg.bvecs --bvals=${workingdir}/Pos_Neg.bvals --fwhm=0 --topup=${topupdir}/topup_Pos_Neg_b0 --out=${workingdir}/eddy_unwarped_images --flm=quadratic -v #--resamp=lsr #--session=${workingdir}/series_index.txt
 
 echo -e "\n END: eddy"
 
