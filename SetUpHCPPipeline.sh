c4783e2875758fd2366e5226479565cde318b301
Tony Wilson <wilsont@mir.wustl.edu>
2013-03-14 17:19:16 -0500

    setup changed

--- SetUpHCPPipeline.sh
+++ SetUpHCPPipeline.sh
@@ -1,39 +1,55 @@
 echo "This script must be SOURCED to correctly setup the environment prior to running any of the other HCP scripts contained here"
 
 # Set up FSL (if not already done so in the running environment)
-FSLDIR=/usr/local/fsl
-. ${FSLDIR}/etc/fslconf/fsl.sh
+FSLDIR=/nrgpackages/scripts
+. ${FSLDIR}/fsl5_setup.sh
 
 # Set up FreeSurfer (if not already done so in the running environment)
-FREESURFER_HOME=/usr/local/freesurfer
+FREESURFER_HOME=/nrgpackages/tools/freesurfer5
 . ${FREESURFER_HOME}/SetUpFreeSurfer.sh
 
 # Set up specific environment variables for the HCP Pipeline
-export HCPPIPEDIR=/vols/Data/HCP/GIT/Pipelines
 # All the following variables can be left as is if the structure of the GIT repository is maintained
-export CARET5DIR=${HCPPIPEDIR}/global/binaries/caret5
-export CARET7DIR=${HCPPIPEDIR}/global/binaries/caret7/bin_linux64
+# export HCPPIPEDIR=/vols/Data/HCP/GIT/Pipelines
+# export CARET5DIR=${HCPPIPEDIR}/global/binaries/caret5
+# export CARET7DIR=${HCPPIPEDIR}/global/binaries/caret7/bin_linux64
 
-export HCPPIPEDIR_Templates=${HCPPIPEDIR}/global/templates
-export HCPPIPEDIR_Bin=${HCPPIPEDIR}/global/binaries
-export HCPPIPEDIR_Config=${HCPPIPEDIR}/global/config
+# export HCPPIPEDIR_Templates=${HCPPIPEDIR}/global/templates
+# export HCPPIPEDIR_Bin=${HCPPIPEDIR}/global/binaries
+# export HCPPIPEDIR_Config=${HCPPIPEDIR}/global/config
 
-export HCPPIPEDIR_PreFS=${HCPPIPEDIR}/PreFreeSurfer/scripts
-export HCPPIPEDIR_FS=${HCPPIPEDIR}/FreeSurfer/scripts
-export HCPPIPEDIR_PostFS=${HCPPIPEDIR}/PostFreeSurfer/scripts
-export HCPPIPEDIR_fMRISurf=${HCPPIPEDIR}/fMRISurface/scripts
-export HCPPIPEDIR_fMRIVol=${HCPPIPEDIR}/fMRIVolume/scripts
-export HCPPIPEDIR_tfMRI=${HCPPIPEDIR}/tfMRI/scripts
-export HCPPIPEDIR_dMRI=${HCPPIPEDIR}/DiffusionPreprocessing/scripts
-export HCPPIPEDIR_Global=${HCPPIPEDIR}/global/scripts
+# export HCPPIPEDIR_PreFS=${HCPPIPEDIR}/PreFreeSurfer/scripts
+# export HCPPIPEDIR_FS=${HCPPIPEDIR}/FreeSurfer/scripts
+# export HCPPIPEDIR_PostFS=${HCPPIPEDIR}/PostFreeSurfer/scripts
+# export HCPPIPEDIR_fMRISurf=${HCPPIPEDIR}/fMRISurface/scripts
+# export HCPPIPEDIR_fMRIVol=${HCPPIPEDIR}/fMRIVolume/scripts
+# export HCPPIPEDIR_tfMRI=${HCPPIPEDIR}/tfMRI/scripts
+# export HCPPIPEDIR_dMRI=${HCPPIPEDIR}/DiffusionPreprocessing/scripts
+# export HCPPIPEDIR_Global=${HCPPIPEDIR}/global/scripts
 
 
 ## WASHU config - as understood by MJ - (different structure from the GIT repository)
-#export HCPPIPEDIR=/data/intradb/pipeline/catalog/FunctionalHCP/resources/scripts/
-#export HCPPIPEDIR_Templates=/nrgpackages/atlas/HCP/
-#export HCPPIPEDIR_Bin=/nrgpackages/tools/HCP/bin/
-#export HCPPIPEDIR_Config=/nrgpackages/tools/HCP/conf
-#export HCPPIPEDIR_Global=/nrgpackages/tools/HCP/scripts/
+## Also look at: /nrgpackages/scripts/tools_setup.sh
+NRG_SCRIPTS=/nrgpackages/scripts
+. ${NRG_SCRIPTS}/epd-python_setup.sh
+
+export HCPPIPEDIR=/home/NRG/jwilso01/dev/Pipelines
+export HCPPIPEDIR_PreFS=${HCPPIPEDIR}/PreFreeSurfer/scripts
+export HCPPIPEDIR_FS=/data/intradb/pipeline/catalog/StructuralHCP/resources/scripts
+export HCPPIPEDIR_PostFS=/data/intradb/pipeline/catalog/StructuralHCP/resources/scripts
+
+export HCPPIPEDIR_FIX=/data/intradb/pipeline/catalog/FIX_HCP/resources/scripts
+export HCPPIPEDIR_Diffusion=/data/intradb/pipeline/catalog/DiffusionHCP/resources/scripts
+export HCPPIPEDIR_Functional=/data/intradb/pipeline/catalog/FunctionalHCP/resources/scripts
+
+export HCPPIPETOOLS=/nrgpackages/tools/HCP
+export HCPPIPEDIR_Templates=/nrgpackages/atlas/HCP
+export HCPPIPEDIR_Bin=${HCPPIPETOOLS}/bin
+export HCPPIPEDIR_Config=${HCPPIPETOOLS}/conf
+export HCPPIPEDIR_Global=${HCPPIPETOOLS}/scripts_v2
+
+export CARET5DIR=${HCPPIPEDIR_Bin}/caret5
+export CARET7DIR=${HCPPIPEDIR_Bin}/caret7/bin_linux64
 ## may or may not want the above variables from CARET5DIR to HCPPIPEDIR_Global to be setup as above or not
 ##    (if so then the HCPPIPEDIR line needs to go before them)
 ## end of WASHU config
