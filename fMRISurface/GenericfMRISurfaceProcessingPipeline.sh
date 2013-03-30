ce4b51d7dc3ce316fe7f2a69c4747046dabe5818
Matt Glasser <glasserm@wusm.wustl.edu>
2013-03-30 13:36:28 -0500

    fMRISurface: *Remove all Caret5 dependencies and incorperate improved wb_command algorithms *Discuss how to handle unique parameters like spacings, turn off gradient distortion correction in example scripts *Convert external interfaces to "MJ Style" *Prepare example data, scripts, and documentation *CleanUp Variable Names for spin echo field map and FcMRI vs fMRI

--- fMRISurface/GenericfMRISurfaceProcessingPipeline.sh
+++ fMRISurface/GenericfMRISurfaceProcessingPipeline.sh
@@ -1,48 +1,91 @@
 #!/bin/bash 
 set -e
 
-Path="$1"
-Subject="$2"
-NameOffMRI="$3"
-DownSampleNameI="$4"
-FinalfMRIResolution="$5"
-SmoothingFWHM="$6"
-Caret5_Command="$7"
-Caret7_Command="$8"
-PipelineScripts="$9"
-AtlasParcellation="${10}"
-AtlasSurfaceROI="${11}"
-BrainOrdinatesResolution="${12}" 
-SubcorticalBrainOrdinatesLabels="${13}"
+# Requirements for this script
+#  installed versions of: FSL5.0.2 or higher , FreeSurfer (version 5 or higher) , gradunwarp (python code from MGH) 
+#  environment: use SetUpHCPPipeline.sh  (or individually set FSLDIR, FREESURFER_HOME, HCPPIPEDIR, PATH - for gradient_unwarp.py)
+
+# make pipeline engine happy...
+if [ $# -eq 1 ]
+then
+    echo "Version unknown..."
+    exit 0
+fi
+
+########################################## PIPELINE OVERVIEW ########################################## 
+
+# TODO
+
+########################################## OUTPUT DIRECTORIES ########################################## 
+
+# TODO
+
+################################################ SUPPORT FUNCTIONS ##################################################
+
+# function for parsing options
+getopt1() {
+    sopt="$1"
+    shift 1
+    for fn in $@ ; do
+	if [ `echo $fn | grep -- "^${sopt}=" | wc -w` -gt 0 ] ; then
+	    echo $fn | sed "s/^${sopt}=//"
+	    return 0
+	fi
+    done
+}
+
+defaultopt() {
+    echo $1
+}
+
+################################################## OPTION PARSING #####################################################
+
+# Just give usage if no arguments specified
+if [ $# -eq 0 ] ; then Usage; exit 0; fi
+
+# parse arguments
+Path=`getopt1 "--path" $@`  # "$1"
+Subject=`getopt1 "--subject" $@`  # "$2"
+NameOffMRI=`getopt1 "--fmriname" $@`  # "$6"
+LowResMesh=`getopt1 "--lowresmesh" $@`  # "$6"
+FinalfMRIResolution=`getopt1 "--fmrires" $@`  # "${14}"
+SmoothingFWHM=`getopt1 "--smoothingFWHM" $@`  # "${14}"
+GrayordinatesResolution=`getopt1 "--grayordinatesres" $@`  # "${14}"
+RUN=`getopt1 "--printcom" $@`  # use ="echo" for just printing everything and not running the commands (default is to run)
+
+
+
+# Setup PATHS
+PipelineScripts=${HCPPIPEDIR_fMRISurf}
+GlobalScripts=${HCPPIPEDIR_Global}
+GlobalBinaries=${HCPPIPEDIR_Bin}
 
 #Naming Conventions
 AtlasSpaceFolder="MNINonLinear"
+T1wFolder="T1w"
 NativeFolder="Native"
 ResultsFolder="Results"
-T1wAtlasName="T1w_restore"
-DownSampleFolder="fsaverage_LR${DownSampleNameI}k"
+DownSampleFolder="fsaverage_LR${LowResMesh}k"
 ROIFolder="ROIs"
-OutputDenseTimeseries="${NameOffMRI}"
 OutputAtlasDenseTimeseries="${NameOffMRI}_Atlas"
 
 AtlasSpaceFolder="$Path"/"$Subject"/"$AtlasSpaceFolder"
+T1wFolder="$Path"/"$Subject"/"$T1wFolder"
 ResultsFolder="$AtlasSpaceFolder"/"$ResultsFolder"/"$NameOffMRI"
 ROIFolder="$AtlasSpaceFolder"/"$ROIFolder"
 
 #Make fMRI Ribbon
 #Noisy Voxel Outlier Exclusion
 #Ribbon-based Volume to Surface mapping and resampling to standard surface
-#Alternates Could Be Trilinear (interpolated voxel) or NearestNeighbor (enclosing voxel)
 mkdir -p "$ResultsFolder"/RibbonVolumeToSurfaceMapping
-"$PipelineScripts"/RibbonVolumeToSurfaceMapping.sh "$ResultsFolder"/RibbonVolumeToSurfaceMapping "$ResultsFolder"/"$NameOffMRI" "$Subject" "$AtlasSpaceFolder"/"$NativeFolder" "$AtlasSpaceFolder"/"$DownSampleFolder" "$AtlasSpaceFolder"/"$T1wAtlasName""$FinalfMRIResolution" "$DownSampleNameI" "$Caret5_Command" "$Caret7_Command" "$AtlasSurfaceROI"
+"$PipelineScripts"/RibbonVolumeToSurfaceMapping.sh "$ResultsFolder"/RibbonVolumeToSurfaceMapping "$ResultsFolder"/"$NameOffMRI" "$Subject" "$AtlasSpaceFolder"/"$DownSampleFolder" "$LowResMesh" "$AtlasSpaceFolder"/"$NativeFolder" "$T1wFolder"/"$NativeFolder"
 
 #Surface Smoothing
-"$PipelineScripts"/SurfaceSmoothing.sh "$ResultsFolder"/"$NameOffMRI" "$Subject" "$AtlasSpaceFolder"/"$DownSampleFolder" "$DownSampleNameI" "$SmoothingFWHM" "$Caret7_Command"
+"$PipelineScripts"/SurfaceSmoothing.sh "$ResultsFolder"/"$NameOffMRI" "$Subject" "$AtlasSpaceFolder"/"$DownSampleFolder" "$LowResMesh" "$SmoothingFWHM"
 
 #Subcortical Processing
-mkdir -p "$ResultsFolder"/SubcorticalProcessing 
-"$PipelineScripts"/SubcorticalProcessing.sh "$ResultsFolder"/SubcorticalProcessing "$AtlasSpaceFolder" "$ROIFolder" "$AtlasParcellation" "$FinalfMRIResolution" "$DownSampleNameI" "$ResultsFolder"/"$NameOffMRI" "$SmoothingFWHM" "$Caret7_Command" "$SubcorticalBrainOrdinatesLabels" "$T1wAtlasName" "$BrainOrdinatesResolution"
+"$PipelineScripts"/SubcorticalProcessing.sh "$AtlasSpaceFolder" "$ROIFolder" "$FinalfMRIResolution" "$ResultsFolder"/"$NameOffMRI" "$SmoothingFWHM" "$GrayordinatesResolution"
 
 #Generation of Dense Timeseries
-"$PipelineScripts"/CreateDenseTimeseries.sh "$AtlasSpaceFolder"/"$DownSampleFolder" "$Subject" "$DownSampleNameI" "$ResultsFolder"/"$NameOffMRI" "$SmoothingFWHM" "$FinalfMRIResolution" "$ROIFolder" "$ResultsFolder"/"$OutputDenseTimeseries" "$ResultsFolder"/"$OutputAtlasDenseTimeseries" "$Caret7_Command" "$BrainOrdinatesResolution"
+"$PipelineScripts"/CreateDenseTimeseries.sh "$AtlasSpaceFolder"/"$DownSampleFolder" "$Subject" "$LowResMesh" "$ResultsFolder"/"$NameOffMRI" "$SmoothingFWHM" "$ROIFolder" "$ResultsFolder"/"$OutputAtlasDenseTimeseries" "$GrayordinatesResolution"
 
