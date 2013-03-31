24ded5e92478632223ea82775d850158cdd65718
Matt Glasser <glasserm@wusm.wustl.edu>
2013-03-30 19:38:32 -0500

    *Bugfix fMRISurface

--- fMRISurface/scripts/SubcorticalProcessing.sh
+++ fMRISurface/scripts/SubcorticalProcessing.sh
@@ -11,7 +11,7 @@ BrainOrdinatesResolution="$6"
 
 Sigma=`echo "$SmoothingFWHM / ( 2 * ( sqrt ( 2 * l ( 2 ) ) ) )" | bc -l`
 
-if [ `echo "$BrainOrdinatesResolution == $FinalfMRIResolution" | bc -l` -eq 1  ] ; then
+if [ `echo "if ( $BrainOrdinatesResolution == $FinalfMRIResolution ) { 1 }" | bc -l` -eq 1 ] ; then
   ${CARET7DIR}/wb_command -volume-parcel-resampling "$VolumefMRI".nii.gz "$ROIFolder"/ROIs."$FinalfMRIResolution".nii.gz "$ROIFolder"/Atlas_ROIs."$BrainOrdinatesResolution".nii.gz $Sigma "$VolumefMRI"_AtlasSubcortical_s"$SmoothingFWHM".nii.gz -fix-zeros
 else
   ${CARET7DIR}/wb_command -volume-parcel-resampling-generic "$VolumefMRI".nii.gz "$ROIFolder"/ROIs."$FinalfMRIResolution".nii.gz "$ROIFolder"/Atlas_ROIs."$BrainOrdinatesResolution".nii.gz $Sigma "$VolumefMRI"_AtlasSubcortical_s"$SmoothingFWHM".nii.gz -fix-zeros
