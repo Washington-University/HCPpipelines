be554612a33ec2b12727cb848d410b0e93f43989
Stam Sotiropoulos <stam@fmrib.ox.ac.uk>
2013-05-17 10:42:34 +0100

    Add GradientDistortionUnwarp to diffusion pipeline. Make diffusion resolution a parameter to DiffusionToStructural.

--- DiffusionPreprocessing/scripts/DiffusionToStructural.sh
+++ DiffusionPreprocessing/scripts/DiffusionToStructural.sh
@@ -24,26 +24,29 @@ defaultopt() {
 
 ################################################## OPTION PARSING #####################################################
 # Input Variables
-FreeSurferSubjectFolder=`getopt1 "--t1folder" $@` # "$1" #${StudyFolder}/${Subject}/T1w
-FreeSurferSubjectID=`getopt1 "--subject" $@`      # "$2" #Subject ID
-WorkingDirectory=`getopt1 "--workingdir" $@`      # "$3" #Path to registration working dir, e.g. ${StudyFolder}/${Subject}/Diffusion/reg
-DataDirectory=`getopt1 "--datadiffdir" $@`        # "$4" #Path to diffusion space diffusion data, e.g. ${StudyFolder}/${Subject}/Diffusion/data
-T1wImage=`getopt1 "--t1" $@`                      # "$5" #T1w_acpc_dc image
-T1wRestoreImage=`getopt1 "--t1restore" $@`        # "$6" #T1w_acpc_dc_restore image
-T1wBrainImage=`getopt1 "--t1restorebrain" $@`     # "$7" #T1w_acpc_dc_restore_brain image
-BiasField=`getopt1 "--biasfield" $@`              # "$8" #Bias_Field_acpc_dc
-InputBrainMask=`getopt1 "--brainmask" $@`         # "$9" #Freesurfer Brain Mask, e.g. brainmask_fs
-GdcorrectionFlag=`getopt1 "--gdflag" $@`          # "$10"#Flag for gradient nonlinearity correction (0/1 for Off/On) 
+
+FreeSurferSubjectFolder=`getopt1 "--t1folder" $@`  # "$1" #${StudyFolder}/${Subject}/T1w
+FreeSurferSubjectID=`getopt1 "--subject" $@`       # "$2" #Subject ID
+WorkingDirectory=`getopt1 "--workingdir" $@`       # "$3" #Path to registration working dir, e.g. ${StudyFolder}/${Subject}/Diffusion/reg
+DataDirectory=`getopt1 "--datadiffdir" $@`         # "$4" #Path to diffusion space diffusion data, e.g. ${StudyFolder}/${Subject}/Diffusion/data
+T1wImage=`getopt1 "--t1" $@`                       # "$5" #T1w_acpc_dc image
+T1wRestoreImage=`getopt1 "--t1restore" $@`         # "$6" #T1w_acpc_dc_restore image
+T1wBrainImage=`getopt1 "--t1restorebrain" $@`      # "$7" #T1w_acpc_dc_restore_brain image
+BiasField=`getopt1 "--biasfield" $@`               # "$8" #Bias_Field_acpc_dc
+InputBrainMask=`getopt1 "--brainmask" $@`          # "$9" #Freesurfer Brain Mask, e.g. brainmask_fs
+GdcorrectionFlag=`getopt1 "--gdflag" $@`           # "$10"#Flag for gradient nonlinearity correction (0/1 for Off/On) 
+DiffRes=`getopt1 "--diffresol" $@`                 # "$11"#Diffusion resolution in mm (assume isotropic)
 
 # Output Variables
-T1wOutputDirectory=`getopt1 "--datadiffT1wdir" $@`# "$11" #Path to T1w space diffusion data (for producing output)
-RegOutput=`getopt1 "--regoutput" $@`              # "$12" #Temporary file for sanity checks 
-QAImage=`getopt1 "--QAimage" $@`                  # "$13" #Temporary file for sanity checks 
+T1wOutputDirectory=`getopt1 "--datadiffT1wdir" $@` # "$12" #Path to T1w space diffusion data (for producing output)
+RegOutput=`getopt1 "--regoutput" $@`               # "$13" #Temporary file for sanity checks 
+QAImage=`getopt1 "--QAimage" $@`                   # "$14" #Temporary file for sanity checks 
+
+echo $T1wOutputDirectory
 
 # Paths for scripts etc (uses variables defined in SetUpHCPPipeline.sh)
 GlobalScripts=${HCPPIPEDIR_Global}
 GlobalBinaries=${HCPPIPEDIR_Bin}
-
 
 T1wBrainImageFile=`basename $T1wBrainImage`
 regimg="nodif"
@@ -67,16 +70,16 @@ ${FSLDIR}/bin/convert_xfm -omat "$WorkingDirectory"/str2diff.mat -inverse "$Work
 ${FSLDIR}/bin/applywarp --rel --interp=spline -i "$DataDirectory"/"$regimg" -r "$T1wImage".nii.gz --premat="$WorkingDirectory"/diff2str.mat -o "$WorkingDirectory"/"$regimg"2T1w
 ${FSLDIR}/bin/fslmaths "$WorkingDirectory"/"$regimg"2T1w -div "$BiasField" "$WorkingDirectory"/"$regimg"2T1w_restore
 
-#Quick registration checks
+#Are the next two scripts needed?
 ${FSLDIR}/bin/imcp "$WorkingDirectory"/"$regimg"2T1w_restore "$RegOutput"
 ${FSLDIR}/bin/fslmaths "$T1wRestoreImage".nii.gz -mul "$WorkingDirectory"/"$regimg"2T1w_restore.nii.gz -sqrt "$QAImage"_"$regimg".nii.gz
 
 #Generate 1.25mm structural space for resampling the diffusion data into
-${FSLDIR}/bin/flirt -interp spline -in "$T1wRestoreImage" -ref "$T1wRestoreImage" -applyisoxfm 1.25 -out "$T1wRestoreImage"_1.25
-${FSLDIR}/bin/applywarp --rel --interp=spline -i "$T1wRestoreImage" -r "$T1wRestoreImage"_1.25 -o "$T1wRestoreImage"_1.25
+${FSLDIR}/bin/flirt -interp spline -in "$T1wRestoreImage" -ref "$T1wRestoreImage" -applyisoxfm ${DiffRes} -out "$T1wRestoreImage"_${DiffRes}
+${FSLDIR}/bin/applywarp --rel --interp=spline -i "$T1wRestoreImage" -r "$T1wRestoreImage"_${DiffRes} -o "$T1wRestoreImage"_${DiffRes}
 
 #Generate 1.25mm mask in structural space
-${FSLDIR}/bin/flirt -interp nearestneighbour -in "$InputBrainMask" -ref "$InputBrainMask" -applyisoxfm 1.25 -out "$T1wOutputDirectory"/nodif_brain_mask
+${FSLDIR}/bin/flirt -interp nearestneighbour -in "$InputBrainMask" -ref "$InputBrainMask" -applyisoxfm ${DiffRes} -out "$T1wOutputDirectory"/nodif_brain_mask
 ${FSLDIR}/bin/fslmaths "$T1wOutputDirectory"/nodif_brain_mask -kernel 3D -dilM "$T1wOutputDirectory"/nodif_brain_mask
 
 #Rotate bvecs from diffusion to structural space
@@ -87,15 +90,15 @@ cp "$DataDirectory"/bvals "$T1wOutputDirectory"/bvals
 if [ ${GdcorrectionFlag} -eq 1 ]; then
     echo "Correcting Diffusion data for gradient nonlinearities and registering to structural space"
     #In the future, we want this applywarp to be part of eddy and avoid second resampling step.
-    ${FSLDIR}/bin/convertwarp --rel --relout --warp1="$DataDirectory"/warped/fullWarp --postmat="$WorkingDirectory"/diff2str.mat --ref="$T1wRestoreImage"_1.25 --out="$WorkingDirectory"/grad_unwarp_diff2str
-    ${FSLDIR}/bin/applywarp --rel -i "$DataDirectory"/warped/data_warped -r "$T1wRestoreImage"_1.25 -w "$WorkingDirectory"/grad_unwarp_diff2str --interp=spline -o "$T1wOutputDirectory"/data
+    ${FSLDIR}/bin/convertwarp --rel --relout --warp1="$DataDirectory"/warped/fullWarp --postmat="$WorkingDirectory"/diff2str.mat --ref="$T1wRestoreImage"_${DiffRes} --out="$WorkingDirectory"/grad_unwarp_diff2str
+    ${FSLDIR}/bin/applywarp --rel -i "$DataDirectory"/warped/data_warped -r "$T1wRestoreImage"_${DiffRes} -w "$WorkingDirectory"/grad_unwarp_diff2str --interp=spline -o "$T1wOutputDirectory"/data
 
     #Now register the grad_dev tensor 
-    ${GlobalBinaries}/vecreg -i "$DataDirectory"/grad_dev -o "$T1wOutputDirectory"/grad_dev -r "$T1wRestoreImage"_1.25 -t "$WorkingDirectory"/diff2str.mat --interp=spline
+    ${GlobalBinaries}/vecreg -i "$DataDirectory"/grad_dev -o "$T1wOutputDirectory"/grad_dev -r "$T1wRestoreImage"_${DiffRes} -t "$WorkingDirectory"/diff2str.mat --interp=spline
     ${FSLDIR}/bin/fslmaths "$T1wOutputDirectory"/grad_dev -mas "$T1wOutputDirectory"/nodif_brain_mask "$T1wOutputDirectory"/grad_dev  #Mask-out values outside the brain 
 else
 #Register diffusion data to T1w space without considering gradient nonlinearities
-    ${FSLDIR}/bin/flirt -in "$DataDirectory"/data -ref "$T1wRestoreImage"_1.25 -applyxfm -init "$WorkingDirectory"/diff2str.mat -interp spline -out "$T1wOutputDirectory"/data
+    ${FSLDIR}/bin/flirt -in "$DataDirectory"/data -ref "$T1wRestoreImage"_${DiffRes} -applyxfm -init "$WorkingDirectory"/diff2str.mat -interp spline -out "$T1wOutputDirectory"/data
 fi
 
 ${FSLDIR}/bin/fslmaths "$T1wOutputDirectory"/data -mas "$T1wOutputDirectory"/nodif_brain_mask "$T1wOutputDirectory"/data  #Mask-out data outside the brain 
