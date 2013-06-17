aaf454933fc3fdb244ae2b20c86e41b6d608f7df
Stam Sotiropoulos <stam@fmrib.ox.ac.uk>
2013-06-17 18:31:29 +0100

    Minor bug corrections to pretractography scripts

--- DiffusionTractography/scripts/MakeWorkbenchUODFs.sh
+++ DiffusionTractography/scripts/MakeWorkbenchUODFs.sh
@@ -32,14 +32,14 @@ Caret7_Command=${CARET7DIR}/wb_command
 
 #NamingConventions and Paths
 trajectory="Whole_Brain_Trajectory"
-T1wDiffusionFolder="${StudyFolder}/${Subject}/T1w/Diffusion"
+T1wFolder="${StudyFolder}/${Subject}/T1w"
 BedpostXFolder="${StudyFolder}/${Subject}/T1w/Diffusion.bedpostX"
 MNINonLinearFolder="${StudyFolder}/${Subject}/MNINonLinear"
 NativeFolder="${StudyFolder}/${Subject}/T1w/Native"
 DownSampleFolder="${StudyFolder}/${Subject}/T1w/fsaverage_LR${LowResMesh}k"
 
 echo "Creating Fibre File for Connectome Workbench"
-${Caret7_Command} -estimate-fiber-binghams ${BedpostXFolder}/merged_f1samples.nii.gz ${BedpostXFolder}/merged_th1samples.nii.gz ${BedpostXFolder}/merged_ph1samples.nii.gz ${BedpostXFolder}/merged_f2samples.nii.gz ${BedpostXFolder}/merged_th2samples.nii.gz ${BedpostXFolder}/merged_ph2samples.nii.gz ${BedpostXFolder}/merged_f3samples.nii.gz ${BedpostXFolder}/merged_th3samples.nii.gz ${BedpostXFolder}/merged_ph3samples.nii.gz ${T1wDiffusionFolder}/${trajectory}_${DiffusionResolution}.nii.gz ${BedpostXFolder}/${trajectory}_${DiffusionResolution}.fiberTEMP.nii
+${Caret7_Command} -estimate-fiber-binghams ${BedpostXFolder}/merged_f1samples.nii.gz ${BedpostXFolder}/merged_th1samples.nii.gz ${BedpostXFolder}/merged_ph1samples.nii.gz ${BedpostXFolder}/merged_f2samples.nii.gz ${BedpostXFolder}/merged_th2samples.nii.gz ${BedpostXFolder}/merged_ph2samples.nii.gz ${BedpostXFolder}/merged_f3samples.nii.gz ${BedpostXFolder}/merged_th3samples.nii.gz ${BedpostXFolder}/merged_ph3samples.nii.gz ${T1wFolder}/${trajectory}_${DiffusionResolution}.nii.gz ${BedpostXFolder}/${trajectory}_${DiffusionResolution}.fiberTEMP.nii
 
 ${Caret7_Command} -add-to-spec-file ${NativeFolder}/${Subject}.native.wb.spec INVALID ${BedpostXFolder}/${trajectory}_${DiffusionResolution}.fiberTEMP.nii
 ${Caret7_Command} -add-to-spec-file ${DownSampleFolder}/${Subject}.${LowResMesh}k_fs_LR.wb.spec INVALID ${BedpostXFolder}/${trajectory}_${DiffusionResolution}.fiberTEMP.nii
