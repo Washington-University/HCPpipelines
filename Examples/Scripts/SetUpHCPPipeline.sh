97b994a4736cd35a1bc3d9c2ff422da2f3a341a7
Matt Glasser <glasserm@wusm.wustl.edu>
2013-12-11 18:37:02 -0600

    PostFreeSurfer Changes: *Add MSMSulc as an optional registration method.  Note that MSM must be installed (not included in the repository) and its path set in the environment variable script Examples/Scripts/SetUpHCPPipeline.sh.  Also add FreeSurfer and MSMSulc distortion maps *Add ability to use multiple low resolution meshes and grayordinates voxel spaces (may be needed for 7T processing).  These must be defined in advance and put in the templates folders. *Some code cleanup *Fix zeros in the greyordinates space because of bad medial wall definition bug *Add standard flat maps to spec files (if available) *Fix bug where left and right hemisphere FreeSurfer labels were combined in grayordinates space *Optionally allow myelin map bias field correction smoothing to be varied (default is previous hardcoded value) *Fix bug in adaptive barycentric resampling algorithm (i.e. new version of wb_command in previous commit)

--- Examples/Scripts/SetUpHCPPipeline.sh
+++ Examples/Scripts/SetUpHCPPipeline.sh
@@ -30,6 +30,7 @@ export HCPPIPEDIR_dMRI=${HCPPIPEDIR}/DiffusionPreprocessing/scripts
 export HCPPIPEDIR_dMRITract=${HCPPIPEDIR}/DiffusionTractography/scripts
 export HCPPIPEDIR_Global=${HCPPIPEDIR}/global/scripts
 export HCPPIPEDIR_tfMRIAnalysis=${HCPPIPEDIR}/TaskfMRIAnalysis/scripts
+export MSMBin=${HCPPIPEDIR}/MSMBinaries
 
 ## WASHU config - as understood by MJ - (different structure from the GIT repository)
 ## Also look at: /nrgpackages/scripts/tools_setup.sh
