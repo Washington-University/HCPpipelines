66200c8907e4944fbe72107401a8b45b8a39b51b
Timothy Brown <tbbrown@wustl.edu>
2014-01-21 14:54:34 -0600

    DiffusionPreprocessing/scripts/run_topup.sh * No longer uses custom version of applytopup, uses released version   in ${FSLDIR}/bin

--- DiffusionPreprocessing/scripts/run_topup.sh
+++ DiffusionPreprocessing/scripts/run_topup.sh
@@ -5,7 +5,6 @@ echo -e "\n START: run_topup"
 
 workingdir=$1
 
-binarydir=${HCPPIPEDIR_Bin}
 configdir=${HCPPIPEDIR_Config}
 #topup_config_file=${FSLDIR}/etc/flirtsch/b02b0.cnf
 topup_config_file=${configdir}/b02b0.cnf
@@ -18,7 +17,7 @@ dimt=$((${dimt} + 1))
 echo "Applying topup to get a hifi b0"
 ${FSLDIR}/bin/fslroi ${workingdir}/Pos_b0 ${workingdir}/Pos_b01 0 1
 ${FSLDIR}/bin/fslroi ${workingdir}/Neg_b0 ${workingdir}/Neg_b01 0 1
-${binarydir}/applytopup --imain=${workingdir}/Pos_b01,${workingdir}/Neg_b01 --topup=${workingdir}/topup_Pos_Neg_b0 --datain=${workingdir}/acqparams.txt --inindex=1,${dimt} --out=${workingdir}/hifib0
+${FSLDIR}/bin/applytopup --imain=${workingdir}/Pos_b01,${workingdir}/Neg_b01 --topup=${workingdir}/topup_Pos_Neg_b0 --datain=${workingdir}/acqparams.txt --inindex=1,${dimt} --out=${workingdir}/hifib0
 
 ${FSLDIR}/bin/imrm ${workingdir}/Pos_b0*
 ${FSLDIR}/bin/imrm ${workingdir}/Neg_b0*
