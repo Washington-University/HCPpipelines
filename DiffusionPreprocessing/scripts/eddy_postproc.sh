f6bb214846d1af53203586026e472238f5c62fd4
Timothy Brown <tbbrown@wustl.edu>
2014-01-21 15:01:35 -0600

    DiffusionPreprocessing/scripts/eddy_postproc.sh * uses released version of eddy_combine in ${FSLDIR}/bin instead   of custom version

--- DiffusionPreprocessing/scripts/eddy_postproc.sh
+++ DiffusionPreprocessing/scripts/eddy_postproc.sh
@@ -27,7 +27,7 @@ datadir=${workingdir}/data
      NegVols=`wc ${eddydir}/Neg.bval | awk {'print $2'}`    #Split Pos and Neg Volumes
      ${FSLDIR}/bin/fslroi ${eddydir}/eddy_unwarped_images ${eddydir}/eddy_unwarped_Pos 0 ${PosVols}
      ${FSLDIR}/bin/fslroi ${eddydir}/eddy_unwarped_images ${eddydir}/eddy_unwarped_Neg ${PosVols} ${NegVols}
-     ${binarydir}/eddy_combine ${eddydir}/eddy_unwarped_Pos ${eddydir}/Pos.bval ${eddydir}/Pos.bvec ${eddydir}/Pos_SeriesVolNum.txt \
+     ${FSLDIR}/bin/eddy_combine ${eddydir}/eddy_unwarped_Pos ${eddydir}/Pos.bval ${eddydir}/Pos.bvec ${eddydir}/Pos_SeriesVolNum.txt \
                                         ${eddydir}/eddy_unwarped_Neg ${eddydir}/Neg.bval ${eddydir}/Neg.bvec ${eddydir}/Neg_SeriesVolNum.txt ${datadir} 1
 
      ${FSLDIR}/bin/imrm ${eddydir}/eddy_unwarped_Pos
