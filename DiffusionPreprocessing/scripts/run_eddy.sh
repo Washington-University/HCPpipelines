55a604036b2223e6a238b585b85ca0a6daf17267
Timothy Brown <tbbrown@wustl.edu>
2014-01-22 13:35:52 -0600

    DiffusionPreprocessing/scripts/run_eddy.sh - no longer uses custom eddy binary. - uses ${FSLDIR}/bin released version of eddy

--- DiffusionPreprocessing/scripts/run_eddy.sh
+++ DiffusionPreprocessing/scripts/run_eddy.sh
@@ -4,12 +4,11 @@ echo -e "\n START: eddy"
 
 workingdir=$1
 
-binarydir=${HCPPIPEDIR_Bin}
 topupdir=`dirname ${workingdir}`/topup
 
 ${FSLDIR}/bin/imcp ${topupdir}/nodif_brain_mask ${workingdir}/
 
-${binarydir}/eddy --imain=${workingdir}/Pos_Neg --mask=${workingdir}/nodif_brain_mask --index=${workingdir}/index.txt --acqp=${workingdir}/acqparams.txt --bvecs=${workingdir}/Pos_Neg.bvecs --bvals=${workingdir}/Pos_Neg.bvals --fwhm=0 --topup=${topupdir}/topup_Pos_Neg_b0 --out=${workingdir}/eddy_unwarped_images --flm=quadratic -v #--resamp=lsr #--session=${workingdir}/series_index.txt
+${FSLDIR}/bin/eddy --imain=${workingdir}/Pos_Neg --mask=${workingdir}/nodif_brain_mask --index=${workingdir}/index.txt --acqp=${workingdir}/acqparams.txt --bvecs=${workingdir}/Pos_Neg.bvecs --bvals=${workingdir}/Pos_Neg.bvals --fwhm=0 --topup=${topupdir}/topup_Pos_Neg_b0 --out=${workingdir}/eddy_unwarped_images --flm=quadratic -v #--resamp=lsr #--session=${workingdir}/series_index.txt
 
 echo -e "\n END: eddy"
 
