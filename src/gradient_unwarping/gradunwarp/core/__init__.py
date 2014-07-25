70bcdb4cd529ed7f9f1f0d4bf74d661c895e6793
Timothy Brown <tbbrown@wustl.edu>
2014-07-25 15:10:03 -0500

    Removed embedded gradient unwarping code. Moved to its own repository github.com/Washington-University/gradunwarp

--- src/gradient_unwarping/gradunwarp/core/__init__.py
+++
@@ -1,7 +0,0 @@
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-#
-#   See COPYING file distributed along with the gradunwarp package for the
-#   copyright and license terms.
-#
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-from unwarp_resample import Unwarper
