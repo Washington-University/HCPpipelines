8c6c6f97ab3c49ca5a003f94d47d00d3dedc5a0e
Timothy Brown <tbbrown@wustl.edu>
2014-08-11 15:52:42 -0500

    Added debugging output to run_topup.sh script

--- DiffusionPreprocessing/scripts/run_topup.sh
+++ DiffusionPreprocessing/scripts/run_topup.sh
@@ -19,11 +19,23 @@ ${FSLDIR}/bin/fslroi ${workingdir}/Pos_b0 ${workingdir}/Pos_b01 0 1
 ${FSLDIR}/bin/fslroi ${workingdir}/Neg_b0 ${workingdir}/Neg_b01 0 1
 ${FSLDIR}/bin/applytopup --imain=${workingdir}/Pos_b01,${workingdir}/Neg_b01 --topup=${workingdir}/topup_Pos_Neg_b0 --datain=${workingdir}/acqparams.txt --inindex=1,${dimt} --out=${workingdir}/hifib0
 
+if [ ! -f ${workingdir}/hifib0.nii.gz ]; then
+    echo "run_topup.sh -- ERROR -- ${FSLDIR}/bin/applytopup failed to generate ${workingdir}/hifib0.nii.gz"
+    # Need to add mechanism whereby scripts that invoke this script (run_topup.sh)
+    # check for a return code to determine success or failure
+fi
+
 ${FSLDIR}/bin/imrm ${workingdir}/Pos_b0*
 ${FSLDIR}/bin/imrm ${workingdir}/Neg_b0*
 
 echo "Running BET on the hifi b0"
 ${FSLDIR}/bin/bet ${workingdir}/hifib0 ${workingdir}/nodif_brain -m -f 0.2
+
+if [ ! -f ${workingdir}/nodif_brain ]; then
+    echo "run_topup.sh -- ERROR -- ${FSLDIR}/bin/bet failed to generate ${workingdir}/nodif_brain.nii.gz"
+    # Need to add mechanism whereby scripts that invoke this script (run_topup.sh)
+    # check for a return code to determine success or failure
+fi
 
 echo -e "\n END: run_topup"
 
