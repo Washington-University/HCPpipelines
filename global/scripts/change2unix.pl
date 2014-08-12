82c6416be2e098c07a50bebebe4ddcbbbaf4a233
Timothy Brown <tbbrown@wustl.edu>
2014-08-12 13:04:11 -0500

    Moved files associated with level 3 task analysis to level3-task-fmri-analysis branch

--- global/scripts/change2unix.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-while (<STDIN>) {
-        s/\r\n/\n/gi;	 # replace DOS returns with Unix newlines
-        s/\r/\n/gi ;     # replace Mac returns with Unix newlines
-        print $_ ;
-}
