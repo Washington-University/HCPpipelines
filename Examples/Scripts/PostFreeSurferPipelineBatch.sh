8741b71342af9a868dc084f127312d9fffb04d49
Timothy Brown <tbbrown@wustl.edu>
2014-08-26 16:00:45 -0500

    Having get_batch_options.shlib as a separate library of functions doesn't seem to be working under Ubuntu

--- Examples/Scripts/PostFreeSurferPipelineBatch.sh
+++ Examples/Scripts/PostFreeSurferPipelineBatch.sh
@@ -1,7 +1,36 @@
 #!/bin/bash 
 
-curdir=`dirname ${BASH_SOURCE[0]}`
-source ${curdir}/get_batch_options.shlib # get_batch_options function
+get_batch_options() {
+    local arguments=($@)
+
+    unset command_line_specified_study_folder
+    unset command_line_specified_subj_list
+    unset command_line_specified_run_local
+
+    local index=0
+    local numArgs=${#arguments[@]}
+    local argument
+
+    while [ ${index} -lt ${numArgs} ]; do
+        argument=${arguments[index]}
+
+        case ${argument} in
+            --StudyFolder=*)
+                command_line_specified_study_folder=${argument/*=/""}
+                index=$(( index + 1 ))
+                ;;
+            --Subjlist=*)
+                command_line_specified_subj_list=${argument/*=/""}
+                index=$(( index + 1 ))
+                ;;
+            --runlocal)
+                command_line_specified_run_local="TRUE"
+                index=$(( index + 1 ))
+                ;;
+        esac
+    done
+}
+
 get_batch_options $@
 
 StudyFolder="${HOME}/projects/Pipelines_ExampleData" #Location of Subject folders (named by subjectID)
