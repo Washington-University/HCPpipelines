ff305d225a7e0b4511a7002525eb3617b04bfb44
Lennart Verhagen <lennartverhagen@users.noreply.github.com>
2015-07-26 21:17:13 +0100

    artery detection
    Detect and mask arteries, especially prominent in 7T T1w images.
    Arteries can be ridiculously bright, forcing FNIRT and other
    intensity-based registration algorithms to adopt erroneous
    transformations/warps to compensate.

--- PreFreeSurfer/scripts/ArteryDetection.sh
+++ PreFreeSurfer/scripts/ArteryDetection.sh
@@ -10,9 +10,9 @@ set -e
 ################################################ SUPPORT FUNCTIONS ##################################################
 
 Usage() {
-  echo "`basename $0`: Detect veins in 7T T1w images (optionally with the help of T2w images)"
+  echo "`basename $0`: Detect arteries in 7T T1w images (optionally with the help of T2w images)"
   echo " "
-  echo "Usage: `basename $0` [--workingdir=<working dir>] --t1=<input T1w image> --t1brain=<input T1w brain image> [--brainmaskloose=<loose brain mask for the veins>] [--exclmask=<exclusion mask (e.g. WM) image>] [--basename=<output base name>]"
+  echo "Usage: `basename $0` [--workingdir=<working dir>] --t1=<input T1w image> --t1brain=<input T1w brain image> [--brainmaskloose=<loose brain mask for the arteries>] [--exclmask=<exclusion mask (e.g. WM) image>] [--basename=<output base name>]"
 }
 
 # function for parsing options
@@ -52,7 +52,7 @@ BaseT1w=$(${FSLDIR}/bin/remove_ext $T1w)
 BaseT1w=$(basename "$BaseT1w")
 
 # default parameters
-WD=$(defaultopt $WD ./VeinDetection)
+WD=$(defaultopt $WD ./ArteryDetection)
 BaseOut=$(defaultopt $BaseOut $BaseT1w)
 
 # ensure that the working directory exists
@@ -60,7 +60,7 @@ mkdir -p $WD
 
 # reporting
 echo " "
-echo " START: Vein Detection"
+echo " START: Artery Detection"
 echo " "
 
 # Record the input options in a log file
@@ -85,13 +85,13 @@ fi
 T1wBrainLoose=$WD/${BaseOut}_brainloose
 ${FSLDIR}/bin/fslmaths $T1w -mas $BrainMaskLoose $T1wBrainLoose
 
-echo "  finding veins based on their intensity in the T1w image"
+echo "  finding arteries based on their intensity in the T1w image"
 # find the threshold for the 10 and 1 percent brightest voxels
 thr90=$(${FSLDIR}/bin/fslstats $T1wBrain -P 90)
 thr99=$(${FSLDIR}/bin/fslstats $T1wBrain -P 99)
 
 if [[ -z $ExclMask ]] ; then
-  echo "  Not excluding any voxels from the vein mask (such as WM voxels). This is potentially problematic, so stricter vein detection criteria are applied."
+  echo "  Not excluding any voxels from the artery mask (such as WM voxels). This is potentially problematic, so stricter artery detection criteria are applied."
   cluster90thr=200
   cluster99thr=100
 else
@@ -110,37 +110,37 @@ ${FSLDIR}/bin/fslmaths $WD/${BaseOut}_thr90cs -mul $WD/${BaseOut}_thr90cm -div $
 ${FSLDIR}/bin/fslmaths $WD/${BaseOut}_thr99cs -mul $WD/${BaseOut}_thr99cm -div $thr99 $WD/${BaseOut}_thr99csm
 
 # exclude clusters with a very low statistic
-${FSLDIR}/bin/fslmaths $WD/${BaseOut}_thr90csm -thr $cluster90thr -bin $WD/${BaseOut}_vein90
-${FSLDIR}/bin/fslmaths $WD/${BaseOut}_thr99csm -thr $cluster99thr -bin $WD/${BaseOut}_vein99
+${FSLDIR}/bin/fslmaths $WD/${BaseOut}_thr90csm -thr $cluster90thr -bin $WD/${BaseOut}_artery90
+${FSLDIR}/bin/fslmaths $WD/${BaseOut}_thr99csm -thr $cluster99thr -bin $WD/${BaseOut}_artery99
 
 # add voxels with extreme values, regardless of their cluster extent
 
-echo "  dilating vein skeletons"
+echo "  dilating artery skeletons"
 # repeatedly dilate the high-threshold clusters one voxel into the low-intensity clusters
-${FSLDIR}/bin/fslmaths $WD/${BaseOut}_vein99 -dilF -mas $WD/${BaseOut}_vein90 $WD/${BaseOut}_veinmask
-${FSLDIR}/bin/fslmaths $WD/${BaseOut}_veinmask -dilF -mas $WD/${BaseOut}_vein90 $WD/${BaseOut}_veinmask
-${FSLDIR}/bin/fslmaths $WD/${BaseOut}_veinmask -dilF -mas $WD/${BaseOut}_vein90 $WD/${BaseOut}_veinmask
+${FSLDIR}/bin/fslmaths $WD/${BaseOut}_artery99 -dilF -mas $WD/${BaseOut}_artery90 $WD/${BaseOut}_arterymask
+${FSLDIR}/bin/fslmaths $WD/${BaseOut}_arterymask -dilF -mas $WD/${BaseOut}_artery90 $WD/${BaseOut}_arterymask
+${FSLDIR}/bin/fslmaths $WD/${BaseOut}_arterymask -dilF -mas $WD/${BaseOut}_artery90 $WD/${BaseOut}_arterymask
 
 # dilate the whole mask, regardless of low-intensity clusters
-${FSLDIR}/bin/fslmaths $WD/${BaseOut}_veinmask -dilF $WD/${BaseOut}_veinmaskdil
+${FSLDIR}/bin/fslmaths $WD/${BaseOut}_arterymask -dilF $WD/${BaseOut}_arterymaskdil
 
-# invert the masks to define voxels that are not veins
-${FSLDIR}/bin/fslmaths $WD/${BaseOut}_veinmask -sub 1 -mul -1 $WD/${BaseOut}_veinmask_inv
-${FSLDIR}/bin/fslmaths $WD/${BaseOut}_veinmaskdil -sub 1 -mul -1 $WD/${BaseOut}_veinmaskdil_inv
+# invert the masks to define voxels that are not arteries
+${FSLDIR}/bin/fslmaths $WD/${BaseOut}_arterymask -sub 1 -mul -1 $WD/${BaseOut}_arterymask_inv
+${FSLDIR}/bin/fslmaths $WD/${BaseOut}_arterymaskdil -sub 1 -mul -1 $WD/${BaseOut}_arterymaskdil_inv
 
 # cleanup intermediate images
-${FSLDIR}/bin/imrm $T1wBrainLoose $WD/${BaseOut}_thr90cs $WD/${BaseOut}_thr90cm $WD/${BaseOut}_thr90csm $WD/${BaseOut}_thr99cs $WD/${BaseOut}_thr99cm $WD/${BaseOut}_thr99csm $WD/${BaseOut}_vein90 $WD/${BaseOut}_vein99
+${FSLDIR}/bin/imrm $T1wBrainLoose $WD/${BaseOut}_thr90cs $WD/${BaseOut}_thr90cm $WD/${BaseOut}_thr90csm $WD/${BaseOut}_thr99cs $WD/${BaseOut}_thr99cm $WD/${BaseOut}_thr99csm $WD/${BaseOut}_artery90 $WD/${BaseOut}_artery99
 
 # reporting
 echo " "
-echo " END: Vein Detection"
+echo " END: Artery Detection"
 echo " END: `date`" >> $WD/log.txt
 
 ########################################## QA STUFF ##########################################
 if [ -e $WD/qa.txt ] ; then rm -f $WD/qa.txt ; fi
 echo "cd `pwd`" >> $WD/qa.txt
 echo "# Look at the quality of the bias corrected output (T1w is brain only)" >> $WD/qa.txt
-echo "fslview $T1w $WD/${BaseOut}_veinmask" >> $WD/qa.txt
+echo "fslview $T1w $WD/${BaseOut}_arterymask" >> $WD/qa.txt
 
 
 ##############################################################################################
