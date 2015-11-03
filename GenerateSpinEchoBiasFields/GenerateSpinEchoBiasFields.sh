1c95fcfd4c44315ed4125c60d79148715847fffb
Timothy B. Brown <tbbrown@wustl.edu>
2015-11-03 09:44:59 -0600

    Working version of GenerateSpinEchoBiasFields.sh

--- GenerateSpinEchoBiasFields/GenerateSpinEchoBiasFields.sh
+++ GenerateSpinEchoBiasFields/GenerateSpinEchoBiasFields.sh
@@ -124,12 +124,15 @@ main()
 	Subject=${g_subject}
 	log_Msg "Subject: ${Subject}"
 
+
+	rfMRINames=${g_rfmri_names}
 	rfMRINames=`echo "$rfMRINames" | sed s/"@"/" "/g`
 	if [ "${rfMRINames}" = "NONE" ] ; then
 		rfMRINames=""
 	fi
 	log_Msg "After delimeter substitution, rfMRINames: ${rfMRINames}"
 
+	tfMRINames=${g_tfmri_names}
 	tfMRINames=`echo "$tfMRINames" | sed s/"@"/" "/g`
 	if [ "${tfMRINames}" = "NONE" ] ; then
 		tfMRINames=""
@@ -137,14 +140,15 @@ main()
 	log_Msg "After delimeter substitution, tfMRINames: ${tfMRINames}"
 
 	for fMRIName in ${rfMRINames} ${tfMRINames} ; do
+		log_Msg "fMRIName: ${fMRIName}"
 
 		files=""
 		files+="PhaseOne_gdc_dc.nii.gz "
 		files+="PhaseTwo_gdc_dc.nii.gz "
 		files+="SBRef_dc.nii.gz"
 
-		for File in files ; do
-			log_Msg "File: ${File}"
+		for File in ${files} ; do
+			log_Msg "--File: ${File}"
 			
 			if [ ! -e ${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import/${File} ]; then
 				if [ ! -e ${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import ]; then
@@ -154,37 +158,43 @@ main()
 					mkdir -p ${StudyFolder}/${Subject}/MNINonLinear/Results/${fMRIName}/Import
 				fi
 
-				log_Msg "Copy"
+				log_Msg "----Copy"
 				cp \
 					${StudyFolder}/${Subject}/${fMRIName}/DistortionCorrectionAndEPIToT1wReg_FLIRTBBRAndFreeSurferBBRbased/FieldMap/${File} \
 					${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import/${File}
 
-				log_Msg "convert_xfm"
+				log_Msg "----convert_xfm"
 				${FSLDIR}/bin/convert_xfm -omat \
 					${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import/fMRI2str.mat \
 					-concat ${StudyFolder}/${Subject}/${fMRIName}/DistortionCorrectionAndEPIToT1wReg_FLIRTBBRAndFreeSurferBBRbased/fMRI2str.mat \
-					${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/DistortionCorrectionAndEPIToT1wReg_FLIRTBBRAndFreeSurferBBRbased/Scout_gdc_undistorted.mat
+					        ${StudyFolder}/${Subject}/${fMRIName}/DistortionCorrectionAndEPIToT1wReg_FLIRTBBRAndFreeSurferBBRbased/Scout_gdc_undistorted.mat
 
-				log_Msg "applywarp 1"
+				log_Msg "----applywarp 1"
 				${FSLDIR}/bin/applywarp --interp=spline \
 					-i ${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import/${File} \
 					-r ${StudyFolder}/${Subject}/MNINonLinear/Results/${fMRIName}/${fMRIName}_SBRef.nii.gz \
 					--premat=${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import/fMRI2str.mat \
 					-w ${StudyFolder}/${Subject}/MNINonLinear/xfms/acpc_dc2standard.nii.gz \
 					-o ${StudyFolder}/${Subject}/MNINonLinear/Results/${fMRIName}/Import/${File}
-				
-				log_Msg "applywarp 2"
+
+				log_Msg "----applywarp 2"
 				${FSLDIR}/bin/applywarp --interp=spline \
 					-i ${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import/${File} \
 					-r ${StudyFolder}/${Subject}/T1w/T2w_acpc_dc.nii.gz \
 					--premat=${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import/fMRI2str.mat \
 					-o ${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import/${File}
-					
+
 			else
-				echo "File already exists"
+				echo "--File already exists"
 			fi
 
 		done # File in ${files}
+
+		mv ${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import/* ${StudyFolder}/${Subject}/T1w/Results/${fMRIName}
+		rmdir ${StudyFolder}/${Subject}/T1w/Results/${fMRIName}/Import
+
+		mv ${StudyFolder}/${Subject}/MNINonLinear/Results/${fMRIName}/Import/* ${StudyFolder}/${Subject}/MNINonLinear/Results/${fMRIName}
+		rmdir ${StudyFolder}/${Subject}/MNINonLinear/Results/${fMRIName}/Import
 
 	done # fMRIName in ${rfMRINames} ${tfMRINames}
 
