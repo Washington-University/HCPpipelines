f01b18e43b2fd94d3cd300d37785671a4a6adb90
Matt Glasser <glasserm@wusm.wustl.edu>
2017-01-10 10:54:02 -0600

    *Get MSMAll Pipeline ready for beta release by adding back in interpreted matlab pathway, updating code to my latest version, making example batch script and adding new configs

--- Examples/Scripts/SetUpHCPPipeline.sh
+++ Examples/Scripts/SetUpHCPPipeline.sh
@@ -17,8 +17,8 @@ export FSL_DIR="${FSLDIR}"
 #source ${FREESURFER_HOME}/SetUpFreeSurfer.sh > /dev/null 2>&1
 
 # Set up specific environment variables for the HCP Pipeline
-export HCPPIPEDIR=${HOME}/projects/Pipelines
-export CARET7DIR=${HOME}/tools/workbench/bin_rh_linux64
+export HCPPIPEDIR=/media/myelin/brainmappers/Connectome_Project/Pipelines
+export CARET7DIR=/home/brainmappers/workbench
 
 export HCPPIPEDIR_Templates=${HCPPIPEDIR}/global/templates
 export HCPPIPEDIR_Bin=${HCPPIPEDIR}/global/binaries
