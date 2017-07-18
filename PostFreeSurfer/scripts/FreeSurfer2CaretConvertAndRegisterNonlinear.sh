b17d17482a9bc7084e7c97aa09bae9fce68e665d
Matt Glasser <glasserm@wusm.wustl.edu>
2017-07-18 16:59:19 -0500

    *Remove mistaken comment lines on MSMSulc dir

--- PostFreeSurfer/scripts/FreeSurfer2CaretConvertAndRegisterNonlinear.sh
+++ PostFreeSurfer/scripts/FreeSurfer2CaretConvertAndRegisterNonlinear.sh
@@ -318,12 +318,12 @@ for Hemisphere in L R ; do
 	#If desired, run MSMSulc folding-based registration to FS_LR initialized with FS affine
 	if [ ${RegName} = "MSMSulc" ] ; then
 		#Calculate Affine Transform and Apply
-		#if [ ! -e "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc ] ; then
-		#  mkdir "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc
-		#else
-		#  rm -r "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc
-		#  mkdir "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc
-		#fi
+		if [ ! -e "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc ] ; then
+		  mkdir "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc
+		else
+		  rm -r "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc
+		  mkdir "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc
+		fi
 		${CARET7DIR}/wb_command -surface-affine-regression "$AtlasSpaceFolder"/"$NativeFolder"/${Subject}.${Hemisphere}.sphere.native.surf.gii "$AtlasSpaceFolder"/"$NativeFolder"/${Subject}.${Hemisphere}.sphere.reg.reg_LR.native.surf.gii "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc/${Hemisphere}.mat
 		${CARET7DIR}/wb_command -surface-apply-affine "$AtlasSpaceFolder"/"$NativeFolder"/${Subject}.${Hemisphere}.sphere.native.surf.gii "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc/${Hemisphere}.mat "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc/${Hemisphere}.sphere_rot.surf.gii
 		${CARET7DIR}/wb_command -surface-modify-sphere "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc/${Hemisphere}.sphere_rot.surf.gii 100 "$AtlasSpaceFolder"/"$NativeFolder"/MSMSulc/${Hemisphere}.sphere_rot.surf.gii
