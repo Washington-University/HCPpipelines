2861575f1798be5a3fc26993aa701afe4ccbec77
Michael Harms <mharms@wustl.edu>
2018-12-21 18:52:00 -0600

    Add support for polynomial detrending to functionhighpassandvariancenormalize

--- ICAFIX/functionhighpassandvariancenormalize.m
+++ ICAFIX/functionhighpassandvariancenormalize.m
@@ -1,29 +1,113 @@
-function [ output_args ] = functionhighpassandvariancenormalize(TR,hp,fmri,WBC,varargin)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+function functionhighpassandvariancenormalize(TR,hp,fmri,WBC,varargin)
+%
+% FUNCTIONHIGHPASSANDVARIANCENORMALIZE(TR,HP,FMRI,WBC,[REGSTRING],[POLYDETREND])
+% 
+% This function:
+% (1) Detrends / high-pass filters motion confounds, NIFTI (volume) and CIFTI files
+% (2) Calls icaDim.m to estimate the dimensionalities of structured and unstructured noise,
+%     and compute a spatial variance normalization map.
+% It is written to support 'hcp_fix_multi_run', and is not intended as a general (stand-alone)
+% function supporting detrending and variance normalization.
+%
+% TR: repetition time between frames, in sec
+% HP: determines what high-pass filtering to apply to the motion confounds and data
+%     HP<0: No highpass applied (demeaning only). No "hp" string will be added as part of the file names.
+%     HP>0: If POLYDETREND flag is not set (see below), then HP specifies the full-width (2*sigma), in sec,
+%           to apply using 'fslmaths -bptf'.
+%           If POLYDETREND flag is set, then HP specifies the order of a polynomial detrending, 
+%           and needs to be an integer.
+%     HP=0: If POLYDETREND flag is not set, HP=0 gets interpreted as a linear (1st order) detrend
+%           (this is consistent with the convention in FIX of using HP=0 to specify a linear detrend), 
+%           but the output file names will be named consistent with a 1st order polynomial detrend.
+%           [i.e., "_hppd1", rather than "_hp0"]
+%           If POLYDETREND flag is set, HP=0 gets interpreted as a true 0th order detrend, which is 
+%           the same as demeaning. Mathematically, this is the same as the HP<0 condition,
+%           but the output file will be named differently (i.e., "_hppd0"), and additional
+%           output files will be written.
+% FMRI: The base string of the fmri time series (no extensions)
+% WBC: wb_command (full path)
+% [REGSTRING]: Additional registration-related string to add to output file names. OPTIONAL.
+% [POLYDETREND]: Controls whether value of HP should be interpreted as specifying the order
+%                for a polynomial detrend.  See above for how it impacts the interpretation of HP.
+%                Values: true (or 1) or false (or 0).
+%                OPTIONAL. (Default: false).
+  
+% Authors: M. Glasser and M. Harms
 
-CIFTI=getenv('FSL_FIX_CIFTIRW');
-addpath(CIFTI);
+CIFTIMatlabReaderWriter=getenv('FSL_FIX_CIFTIRW');
+addpath(CIFTIMatlabReaderWriter);
 
-regstring = '';
+%% Defaults
 dovol = 1;
-if length(varargin) > 0 && ~strcmp(varargin{1}, '')
-    dovol = 0;%regname is only used for a new surface registration, so will never require redoing volume
-    regstring = varargin{1};%this has the underscore on the front already
+regstring = '';
+pdflag = false;  % Polynomial detrend
+
+%% Parse varargin
+if length(varargin) >= 1 && ~isempty(varargin{1})
+  dovol = 0; %regname is only used for a new surface registration, so will never require redoing volume
+  regstring = varargin{1}; %this has the underscore on the front already
+  if ~ischar(regstring)
+	error('REGSTRING should be a string');
+  end
+end
+if length(varargin) >= 2 && ~isempty(varargin{2})
+  pdflag = varargin{2};
+end
+if ischar(pdflag)
+  if strcmp(lower(pdflag),'true')
+	pdflag = true;
+  elseif strcmp(lower(pdflag),'false')
+	pdflag = false;
+  else error('Invalid specification of POLYDETREND flag')
+  end
 end
 
+%% Allow for compiled matlab
+if (isdeployed)
+  tr = str2num(tr);
+  hp = str2num(hp);
+end
+
+if pdflag < 0
+  error('Invalid specification of POLYDETREND flag')
+end
+
+%% Argument checking
+if hp==0 && ~pdflag
+  fprintf('hp=0 will be interpreted as polynomial detrending of order 1');
+  pdflag=true;
+  hp=1;
+end
+if pdflag
+  if (~isscalar(hp) || hp < 0 || hp ~= fix(hp))
+	error('hp must be a non-negative integer when requesting polynomial detrending');
+  end
+end
+%% N.B. The above allows for users to specify hp=0, pdflag=true as their input, in which case
+%% a polynomial detrend of order 0 (i.e., remove the mean) will be executed.
+%% That isn't necessary, since demeaning is always performed, but would impact the manner
+%% in which the files are named.
+
+%% Set up the hp string to use in file names
+if pdflag
+  pdstring = 'pd';  % For "polynomial detrend"
+else
+  pdstring = '';
+end
 if hp>=0
-    hpstring = ['_hp' num2str(hp)];
+    hpstring = ['_hp' pdstring num2str(hp)];
 else
     hpstring = '';
 end
 
+%% Load volume time series
 if dovol > 0
     cts=single(read_avw([fmri '.nii.gz']));
     ctsX=size(cts,1); ctsY=size(cts,2); ctsZ=size(cts,3); ctsT=size(cts,4); 
     cts=reshape(cts,ctsX*ctsY*ctsZ,ctsT);
 end
 
+%% Load the motion confounds, and the CIFTI (if hp>=0) (don't need either if hp<0)
 if hp>=0
     confounds=load([fmri hpstring '.ica/mc/prefiltered_func_data_mcf.par']);
     confounds=confounds(:,1:6);
@@ -31,28 +115,25 @@ if hp>=0
     confounds=functionnormalise([confounds confounds.*confounds]);
 
     BO=ciftiopen([fmri '_Atlas' regstring '.dtseries.nii'],WBC);
-else
-    if dovol > 0
-        cts=demean(cts')';
-    end
 end
 
-if hp==0
+%% Apply hp filtering of the motion confounds, volume (if requested), and CIFTI
+if pdflag  % polynomial detrend case
     if dovol > 0
         save_avw(reshape(confounds',size(confounds,2),1,1,size(confounds,1)),[fmri hpstring '.ica/mc/prefiltered_func_data_mcf_conf'],'f',[1 1 1 TR]);
-        confounds=detrend(confounds);
+        confounds=detrendpoly(confounds,hp);
         save_avw(reshape(confounds',size(confounds,2),1,1,size(confounds,1)),[fmri hpstring '.ica/mc/prefiltered_func_data_mcf_conf_hp'],'f',[1 1 1 TR]);
         
-        cts=detrend(cts')';
+        cts=detrendpoly(cts',hp)';
         save_avw(reshape(cts,ctsX,ctsY,ctsZ,ctsT),[fmri hpstring '.nii.gz'],'f',[1 1 1 TR]);
-		% Use -d flag in fslcpgeom even if not technically necessary to reduce possibility of mistakes when editing script
+		% Use -d flag in fslcpgeom (even if not technically necessary) to reduce possibility of mistakes when editing script
         call_fsl(['fslcpgeom ' fmri '.nii.gz ' fmri hpstring '.nii.gz -d']);
     end
     
-    BO.cdata=detrend(BO.cdata')';
+    BO.cdata=detrendpoly(BO.cdata',hp)';
     ciftisave(BO,[fmri '_Atlas' regstring hpstring '.dtseries.nii'],WBC);
-end
-if hp>0
+
+elseif hp>0  % "fslmaths -bptf" based filtering
     if dovol > 0
         save_avw(reshape(confounds',size(confounds,2),1,1,size(confounds,1)),[fmri hpstring '.ica/mc/prefiltered_func_data_mcf_conf'],'f',[1 1 1 TR]);
         call_fsl(sprintf(['fslmaths ' fmri hpstring '.ica/mc/prefiltered_func_data_mcf_conf -bptf %f -1 ' fmri hpstring '.ica/mc/prefiltered_func_data_mcf_conf_hp'],0.5*hp/TR));
@@ -70,9 +151,21 @@ if hp>0
     grot=reshape(single(read_avw('Atlas')),100*BOdimZnew,BOdimT);  BO.cdata=grot(1:BOdimX,:);  clear grot;
     call_fsl('rm Atlas.nii.gz');
     ciftisave(BO,[fmri '_Atlas' regstring hpstring '.dtseries.nii'],WBC);
+
+elseif hp<0  % If no hp filtering, still need to at least demean the volumetric time series
+    if dovol > 0
+	    cts=demean(cts')';
+    end
+
 end
 
-%Compute VN
+
+% NOTE: To interpret the logic of the following "Compute, Save, Apply" sections need to know that this function expects that: 
+% (1) individual run time series will be passed in with hp requested
+% (2) concatenated (multi-run) time series will be passed in with hp<0
+%% [This is a bit of hack: would be cleaner if we disentangled the operation of this function from the specific needs of 'hcp_fix_multi_run'].
+
+%% Compute VN map
 if hp>=0
     if dovol > 0
         Outcts=icaDim(cts,0,1,2,2); %Volume fits two distributions to deal with processing interpolation and multiband
@@ -81,11 +174,12 @@ if hp>=0
     OutBO=icaDim(BO.cdata,0,1,2,3); %CIFTI fits three because of the effects of volume to CIFTI mapping and regularization
 else
     if dovol > 0
-        Outcts=icaDim(cts,0,1,2,3); %Volume fits three distributions to deal with processing interpolation and multiband and concatination effects
+        Outcts=icaDim(cts,0,1,2,3); %Volume fits three distributions to deal with processing interpolation and multiband and concatenation effects
     end
 end
 
-%Save VN
+%% Save VN map
+% (Only need these for the individual runs, which, per above, are expected to be passed in with hp requested).
 if hp>=0
     if dovol > 0
 	    fname=[fmri hpstring '_vn.nii.gz'];
@@ -100,8 +194,9 @@ if hp>=0
     ciftisavereset(VN,fname,WBC);    
 end
 
-%Apply VN and Save HP_VN TCS
-% Note that NIFTI VN'ed TCS gets saved regardless of whether hp>=0
+%% Apply VN and Save HP_VN TCS
+% Here, NIFTI VN'ed TCS gets saved regardless of whether hp>=0 (i.e., regardless of whether individual or concatenated run)
+% But CIFTI version of TCS only saved if hp>0
 if dovol > 0
     cts=cts./repmat(Outcts.noise_unst_std,1,ctsT);
 	% Use '_vnts' (volume normalized time series) as the suffix for the volumetric VN'ed TCS
@@ -131,3 +226,27 @@ end
 
 end
 
+
+%% ----------------------------------------------
+
+%% Polynomial detrending function
+function Y = detrendpoly(X,p);
+  
+  % Check data, must be in column order
+  [m, n] = size(X);
+  if (m == 1)
+	X = X';
+	r=n;
+  else
+	r=m;
+  end
+  
+  if (~isscalar(p) || p < 0 || p ~= fix(p))
+	error('order of polynomial (p) must be a non-negative integer');
+  end
+  
+  b = ((1 : r)' * ones (1, p + 1)) .^ (ones (r, 1) * (0 : p))
+  Y = X - b * (b \ X);
+  
+end
+  
