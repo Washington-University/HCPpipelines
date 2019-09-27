edaee453877cac287e26a89bbbb95a89f8200097
Grega Repovs <grega.repovs@mac.com>
2019-09-27 18:04:52 +0200

    PostFreeSurferPipeline_1res.sh 1/ Changing tabs to spaces, removal of trailing line spaces 2/ Explicit prinrout of parameters 3/ Addition of `--mppversion` parameter and related check / validation code 4/ When T2w image is missing and T2wRestoreImage is set to NONE the following steps are skipped: * Myelin mapping

--- PostFreeSurfer/PostFreeSurferPipeline_1res.sh
+++ PostFreeSurfer/PostFreeSurferPipeline_1res.sh
@@ -12,17 +12,17 @@ set -e
 script_name=$(basename "${0}")
 
 if [ -z "${HCPPIPEDIR}" ]; then
-	echo "${script_name}: ABORTING: HCPPIPEDIR environment variable must be set"
-	exit 1
+  echo "${script_name}: ABORTING: HCPPIPEDIR environment variable must be set"
+  exit 1
 else
-	echo "${script_name}: HCPPIPEDIR: ${HCPPIPEDIR}"
+  echo "${script_name}: HCPPIPEDIR: ${HCPPIPEDIR}"
 fi
 
 if [ -z "${HCPPIPEDIR_PostFS}" ]; then
-	echo "${script_name}: ABORTING: HCPPIPEDIR_PostFS environment variable must be set"
-	exit 1
+  echo "${script_name}: ABORTING: HCPPIPEDIR_PostFS environment variable must be set"
+  exit 1
 else
-	echo "${script_name}: HCPPIPEDIR_PostFS: ${HCPPIPEDIR_PostFS}"
+  echo "${script_name}: HCPPIPEDIR_PostFS: ${HCPPIPEDIR_PostFS}"
 fi
 
 ########################################## PIPELINE OVERVIEW ##########################################
@@ -37,8 +37,8 @@ fi
 #  Load Function Libraries
 # --------------------------------------------------------------------------------
 
-source $HCPPIPEDIR/global/scripts/log.shlib  # Logging related functions
-source $HCPPIPEDIR/global/scripts/opts.shlib # Command line option functions
+source ${HCPPIPEDIR}/global/scripts/log.shlib   # Logging related functions
+source ${HCPPIPEDIR}/global/scripts/opts.shlib  # Command line option functions
 
 ########################################## SUPPORT FUNCTIONS ##########################################
 
@@ -88,6 +88,25 @@ RegName=`opts_DefaultOpt $RegName FS`
 InflateExtraScale=`opts_DefaultOpt $InflateExtraScale 1`
 
 PipelineScripts=${HCPPIPEDIR_PostFS}
+
+#ceho "---> Starting Post FreeSurfer Pipeline"
+echo "---> Starting Post FreeSurfer Pipeline"
+echo " "
+echo " Using parameters ..."
+echo "              --path: ${StudyFolder}"
+echo "           --subject: ${Subject}"
+echo "      --surfatlasdir: ${SurfaceAtlasDIR}"
+echo "  --grayordinatesdir: ${GrayordinatesSpaceDIR}"
+echo "  --grayordinatesres: ${GrayordinatesResolutions}"
+echo "         --hiresmesh: ${HighResMesh}"
+echo "        --lowresmesh: ${LowResMeshes}"
+echo " --subcortgraylabels: ${SubcorticalGrayLabels}"
+echo "  --freesurferlabels: ${FreeSurferLabels}"
+echo "     --refmyelinmaps: ${ReferenceMyelinMaps}"
+echo "           --mcsigma: ${CorrectionSigma}"
+echo "           --regname: ${RegName}"
+echo "   --PipelineScripts: ${PipelineScripts}"
+echo " "
 
 #Naming Conventions
 # Do NOT include spaces in any of these names
@@ -140,6 +159,63 @@ FreeSurferFolder="$T1wFolder"/"$FreeSurferFolder"
 AtlasTransform="$AtlasSpaceFolder"/xfms/"$AtlasTransform"
 InverseAtlasTransform="$AtlasSpaceFolder"/xfms/"$InverseAtlasTransform"
 
+
+# ------------------------------------------------------------------------------
+#  Check MMP Version
+# ------------------------------------------------------------------------------
+
+Compliance="hcp"
+MPPVersion=`opts_GetOpt1 "--mppversion" $@`
+MPPVersion=`opts_DefaultOpt $MPPVersion "hcp"`
+
+if [ "${MPPVersion}" = "legacy" ] ; then
+  log_Msg "Legacy Minimal Preprocessing Pipelines: PostFreeSurferPipeline_1res v.XX"
+  log_Msg "NOTICE: You are using MPP version that enables processing of images that do not"
+  log_Msg "        conform to the HCP specification as described in Glasser et al. (2013)!"
+  log_Msg "        Be aware that if the HCP requirements are not met, the level of data quality"
+  log_Msg "        can not be guaranteed and the Glasser et al. (2013) paper should not be used"
+  log_Msg "        in support of this workflow. A mnauscript with comprehensive evaluation for"
+  log_Msg "        the Legacy MPP workflow is in active preparation and should be appropriately"
+  log_Msg "        cited when published."
+  log_Msg "        "
+  log_Msg "Checking available data"
+else
+  log_Msg "HCP Minimal Preprocessing Pipelines: PostFreeSurferPipeline_1res v.XX"
+  log_Msg "Checking data compliance with HCP MPP"
+fi
+
+# -- T2w image
+
+if [ ! -e ${T2wFolder}/T2w.nii.gz ]; then
+    log_Msg "-> T2w image not present (legacy)"
+    Compliance="legacy"
+    T2wRestoreImage="NONE"
+else
+  log_Msg "-> T2w image present"
+fi
+
+# -- Final evaluation
+
+if [ "${MPPVersion}" = "legacy" ] ; then
+  if [ "${Compliance}" = "legacy" ] ; then
+    log_Msg "Processing will continue using Legacy MPP."
+  else
+    log_Msg "All conditions for the use of HCP MPP are met. Consider using HCP MPP instead of Legacy MPP."
+    log_Msg "Processing will continue using Legacy MPP."
+  fi
+else
+  if [ "${Compliance}" = "legacy" ] ; then
+    log_Msg "User requested HCP MPP. However, compliance check for use of HCP MPP failed."
+    log_Msg "Aborting execution."
+    exit 1
+  else
+    log_Msg "Conditions for the use of HCP MPP are met."
+    log_Msg "Processing will continue using HCP MPP."
+  fi
+fi
+
+
+
 #Conversion of FreeSurfer Volumes and Surfaces to NIFTI and GIFTI and Create Caret Files and Registration
 log_Msg "Conversion of FreeSurfer Volumes and Surfaces to NIFTI and GIFTI and Create Caret Files and Registration"
 log_Msg "RegName: ${RegName}"
@@ -171,48 +247,51 @@ argList+="$InflateExtraScale "         # ${23}
 
 
 #Myelin Mapping
-log_Msg "Myelin Mapping"
-log_Msg "RegName: ${RegName}"
+if [ "${T2wRestoreImage}" = "NONE" ]; then
+    log_Msg "Skipping Myelin Mapping"
+else
+    log_Msg "Myelin Mapping"
+    log_Msg "RegName: ${RegName}"
 
-argList="$StudyFolder "                # ${1}
-argList+="$Subject "
-argList+="$AtlasSpaceFolder "
-argList+="$NativeFolder "
-argList+="$T1wFolder "                 # ${5}
-argList+="$HighResMesh "
-argList+="$LowResMeshes "
-argList+="$T1wFolder"/"$OrginalT1wImage "
-argList+="$T2wFolder"/"$OrginalT2wImage "
-argList+="$T1wFolder"/"$T1wImageBrainMask "           # ${10}
-argList+="$T1wFolder"/xfms/"$InitialT1wTransform "
-argList+="$T1wFolder"/xfms/"$dcT1wTransform "
-argList+="$T2wFolder"/xfms/"$InitialT2wTransform "
-argList+="$T1wFolder"/xfms/"$dcT2wTransform "
-argList+="$T1wFolder"/"$FinalT2wTransform "           # ${15}
-argList+="$AtlasTransform "
-argList+="$T1wFolder"/"$BiasField "
-argList+="$T1wFolder"/"$OutputT1wImage "
-argList+="$T1wFolder"/"$OutputT1wImageRestore "
-argList+="$T1wFolder"/"$OutputT1wImageRestoreBrain "  # ${20}
-argList+="$AtlasSpaceFolder"/"$OutputMNIT1wImage "
-argList+="$AtlasSpaceFolder"/"$OutputMNIT1wImageRestore "
-argList+="$AtlasSpaceFolder"/"$OutputMNIT1wImageRestoreBrain "
-argList+="$T1wFolder"/"$OutputT2wImage "
-argList+="$T1wFolder"/"$OutputT2wImageRestore "       # ${25}
-argList+="$T1wFolder"/"$OutputT2wImageRestoreBrain "
-argList+="$AtlasSpaceFolder"/"$OutputMNIT2wImage "
-argList+="$AtlasSpaceFolder"/"$OutputMNIT2wImageRestore "
-argList+="$AtlasSpaceFolder"/"$OutputMNIT2wImageRestoreBrain "
-argList+="$T1wFolder"/xfms/"$OutputOrigT1wToT1w "     # {30}
-argList+="$T1wFolder"/xfms/"$OutputOrigT1wToStandard "
-argList+="$T1wFolder"/xfms/"$OutputOrigT2wToT1w "
-argList+="$T1wFolder"/xfms/"$OutputOrigT2wToStandard "
-argList+="$AtlasSpaceFolder"/"$BiasFieldOutput "
-argList+="$AtlasSpaceFolder"/"$T1wImageBrainMask "    # {35}  Called T1wMNIImageBrainMask in CreateMyelinMaps_1res.sh
-argList+="$AtlasSpaceFolder"/xfms/"$Jacobian "
-argList+="$ReferenceMyelinMaps "
-argList+="$CorrectionSigma "
-argList+="$RegName "                                  # ${39}
-"$PipelineScripts"/CreateMyelinMaps_1res.sh ${argList}
+    argList="$StudyFolder "                # ${1}
+    argList+="$Subject "
+    argList+="$AtlasSpaceFolder "
+    argList+="$NativeFolder "
+    argList+="$T1wFolder "                 # ${5}
+    argList+="$HighResMesh "
+    argList+="$LowResMeshes "
+    argList+="$T1wFolder"/"$OrginalT1wImage "
+    argList+="$T2wFolder"/"$OrginalT2wImage "
+    argList+="$T1wFolder"/"$T1wImageBrainMask "           # ${10}
+    argList+="$T1wFolder"/xfms/"$InitialT1wTransform "
+    argList+="$T1wFolder"/xfms/"$dcT1wTransform "
+    argList+="$T2wFolder"/xfms/"$InitialT2wTransform "
+    argList+="$T1wFolder"/xfms/"$dcT2wTransform "
+    argList+="$T1wFolder"/"$FinalT2wTransform "           # ${15}
+    argList+="$AtlasTransform "
+    argList+="$T1wFolder"/"$BiasField "
+    argList+="$T1wFolder"/"$OutputT1wImage "
+    argList+="$T1wFolder"/"$OutputT1wImageRestore "
+    argList+="$T1wFolder"/"$OutputT1wImageRestoreBrain "  # ${20}
+    argList+="$AtlasSpaceFolder"/"$OutputMNIT1wImage "
+    argList+="$AtlasSpaceFolder"/"$OutputMNIT1wImageRestore "
+    argList+="$AtlasSpaceFolder"/"$OutputMNIT1wImageRestoreBrain "
+    argList+="$T1wFolder"/"$OutputT2wImage "
+    argList+="$T1wFolder"/"$OutputT2wImageRestore "       # ${25}
+    argList+="$T1wFolder"/"$OutputT2wImageRestoreBrain "
+    argList+="$AtlasSpaceFolder"/"$OutputMNIT2wImage "
+    argList+="$AtlasSpaceFolder"/"$OutputMNIT2wImageRestore "
+    argList+="$AtlasSpaceFolder"/"$OutputMNIT2wImageRestoreBrain "
+    argList+="$T1wFolder"/xfms/"$OutputOrigT1wToT1w "     # {30}
+    argList+="$T1wFolder"/xfms/"$OutputOrigT1wToStandard "
+    argList+="$T1wFolder"/xfms/"$OutputOrigT2wToT1w "
+    argList+="$T1wFolder"/xfms/"$OutputOrigT2wToStandard "
+    argList+="$AtlasSpaceFolder"/"$BiasFieldOutput "
+    argList+="$AtlasSpaceFolder"/"$T1wImageBrainMask "    # {35}  Called T1wMNIImageBrainMask in CreateMyelinMaps_1res.sh
+    argList+="$AtlasSpaceFolder"/xfms/"$Jacobian "
+    argList+="$ReferenceMyelinMaps "
+    argList+="$CorrectionSigma "
+    argList+="$RegName "                                  # ${39}
+    "$PipelineScripts"/CreateMyelinMaps_1res.sh ${argList}
 
 log_Msg "Completed"
