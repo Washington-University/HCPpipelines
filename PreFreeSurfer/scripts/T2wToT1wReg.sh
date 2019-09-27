86fd68f500f690c5e2bd5fe7869675eaa6ac27ce
Grega Repovs <grega.repovs@mac.com>
2019-09-27 15:12:17 +0200

    Tabs replaced with spaces, ending spaces removed All the steps that require T2w image are skipped when T2w image is defined as NONE. It enables processing when no T2w image is available.

--- PreFreeSurfer/scripts/T2wToT1wReg.sh
+++ PreFreeSurfer/scripts/T2wToT1wReg.sh
@@ -37,14 +37,23 @@ OutputT2wTransform="${10}"
 T1wImageBrainFile=`basename "$T1wImageBrain"`
 
 cp "$T1wImageBrain".nii.gz "$WD"/"$T1wImageBrainFile".nii.gz
-${FSLDIR}/bin/epi_reg --epi="$T2wImageBrain" --t1="$T1wImage" --t1brain="$WD"/"$T1wImageBrainFile" --out="$WD"/T2w2T1w
-${FSLDIR}/bin/applywarp --rel --interp=spline --in="$T2wImage" --ref="$T1wImage" --premat="$WD"/T2w2T1w.mat --out="$WD"/T2w2T1w
-${FSLDIR}/bin/fslmaths "$WD"/T2w2T1w -add 1 "$WD"/T2w2T1w -odt float
+
+if [ "${T2wImage}" = "NONE" ] ; then
+    log_Msg "Skipping T2w to T1w registration --- no T2w image."
+else
+    ${FSLDIR}/bin/epi_reg --epi="$T2wImageBrain" --t1="$T1wImage" --t1brain="$WD"/"$T1wImageBrainFile" --out="$WD"/T2w2T1w
+    ${FSLDIR}/bin/applywarp --rel --interp=spline --in="$T2wImage" --ref="$T1wImage" --premat="$WD"/T2w2T1w.mat --out="$WD"/T2w2T1w
+    ${FSLDIR}/bin/fslmaths "$WD"/T2w2T1w -add 1 "$WD"/T2w2T1w -odt float
+fi
+
 cp "$T1wImage".nii.gz "$OutputT1wImage".nii.gz
 cp "$T1wImageBrain".nii.gz "$OutputT1wImageBrain".nii.gz
 ${FSLDIR}/bin/fslmerge -t $OutputT1wTransform "$T1wImage".nii.gz "$T1wImage".nii.gz "$T1wImage".nii.gz
 ${FSLDIR}/bin/fslmaths $OutputT1wTransform -mul 0 $OutputT1wTransform
-cp "$WD"/T2w2T1w.nii.gz "$OutputT2wImage".nii.gz
-${FSLDIR}/bin/convertwarp --relout --rel -r "$OutputT2wImage".nii.gz -w $OutputT1wTransform --postmat="$WD"/T2w2T1w.mat --out="$OutputT2wTransform"
+
+[ "${T2wImage}" != "NONE" ] && cp "$WD"/T2w2T1w.nii.gz "$OutputT2wImage".nii.gz
+[ "${T2wImage}" != "NONE" ] && ${FSLDIR}/bin/convertwarp --relout --rel -r "$OutputT2wImage".nii.gz -w $OutputT1wTransform --postmat="$WD"/T2w2T1w.mat --out="$OutputT2wTransform"
 
 log_Msg "END: T2w2T1Reg"
+
+
