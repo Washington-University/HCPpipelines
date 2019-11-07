4c22f5c61ec9c1f31c6b759789635f1dd5c385cf
Michiel Cottaar <MichielCottaar@protonmail.com>
2019-11-07 16:04:40 +0000

    BUG: don't create Diffusion/qc/qc directory if Diffusion/qc already exists

--- DiffusionPreprocessing/DiffPreprocPipeline_PostEddy.sh
+++ DiffusionPreprocessing/DiffPreprocPipeline_PostEddy.sh
@@ -353,7 +353,8 @@ main()
 		cp -p ${filename} ${to_location}
 	done
 
-  cp -r ${outdir}/qc ${outdirT1w}/qc
+  mkdir -p ${outdirT1w/qc
+  cp ${outdir}/qc/* ${outdirT1w}/qc
   immv ${outdirT1w}/cnr_maps ${outdirT1w}/qc/cnr_maps
 
 	log_Msg "Completed!"
