503dc8a513e7807f9daad3adbc80ea70404ee16e
Michael Harms <mharms@wustl.edu>
2019-11-08 10:11:50 -0600

    Update some of the fix-related comments to reflect changes as of fix 1.06.12

--- ICAFIX/ReApplyFixMultiRunPipeline.sh
+++ ICAFIX/ReApplyFixMultiRunPipeline.sh
@@ -533,6 +533,9 @@ main()
 	# It is for that reason that the code below needs to use separate calls to fix_3_clean, with and without DoVol
 	# as an argument, rather than simply passing in the value of DoVol as set within this script.
 	# Not sure if/when this non-intuitive behavior of fix_3_clean will change, but this is accurate as of fix1.067
+	# UPDATE (11/8/2019): As of FIX 1.06.12, fix_3_clean interprets its 5th argument ("DoVol") in the usual boolean
+	# manner. However, since we already had a work-around to this problem, we will leave the code unchanged so that
+	# we don't need to add a FIX version dependency to the script.
 
 	log_Msg "Use fixlist=$fixlist"
 	
