a194c898bd1d07c6d05771a2af32d7eb853433cd
Tim Coalson <tsc5yc@mst.edu>
2020-01-23 15:27:58 -0600

    As wb >=1.4.0 cifti resample outputs exact zeros past the dilation edge, use a separate dilation step for speed

--- fMRISurface/scripts/SubcorticalProcessing.sh
+++ fMRISurface/scripts/SubcorticalProcessing.sh
@@ -105,28 +105,34 @@ rm -f ${ResultsFolder}/${NameOffMRI}_temp_subject.dtseries.nii
 log_Msg "Generate atlas subcortical template cifti"
 ${CARET7DIR}/wb_command -cifti-create-label ${ResultsFolder}/${NameOffMRI}_temp_template.dlabel.nii -volume "$ROIFolder"/Atlas_ROIs."$BrainOrdinatesResolution".nii.gz "$ROIFolder"/Atlas_ROIs."$BrainOrdinatesResolution".nii.gz
 
-#Also predilate to ensure full atlas CIFTI space has non-zero values
+#As of wb_command 1.4.0 and later, volume predilate is much less important for reducing edge ringing, and could be reduced
 if [[ `echo "${Sigma} > 0" | bc -l | cut -f1 -d.` == "1" ]]
 then
     log_Msg "Smoothing and resampling"
     #this is the whole timeseries, so don't overwrite, in order to allow on-disk writing, then delete temporary
     ${CARET7DIR}/wb_command -cifti-smoothing ${ResultsFolder}/${NameOffMRI}_temp_subject_dilate.dtseries.nii 0 ${Sigma} COLUMN ${ResultsFolder}/${NameOffMRI}_temp_subject_smooth.dtseries.nii -fix-zeros-volume
     #resample, delete temporary
-    ${CARET7DIR}/wb_command -cifti-resample ${ResultsFolder}/${NameOffMRI}_temp_subject_smooth.dtseries.nii COLUMN ${ResultsFolder}/${NameOffMRI}_temp_template.dlabel.nii COLUMN ADAP_BARY_AREA CUBIC ${ResultsFolder}/${NameOffMRI}_temp_atlas.dtseries.nii -volume-predilate 30
+    ${CARET7DIR}/wb_command -cifti-resample ${ResultsFolder}/${NameOffMRI}_temp_subject_smooth.dtseries.nii COLUMN ${ResultsFolder}/${NameOffMRI}_temp_template.dlabel.nii COLUMN ADAP_BARY_AREA CUBIC ${ResultsFolder}/${NameOffMRI}_temp_atlas.dtseries.nii -volume-predilate 10
     rm -f ${ResultsFolder}/${NameOffMRI}_temp_subject_smooth.dtseries.nii
 else
     log_Msg "Resampling"
-    ${CARET7DIR}/wb_command -cifti-resample ${ResultsFolder}/${NameOffMRI}_temp_subject_dilate.dtseries.nii COLUMN ${ResultsFolder}/${NameOffMRI}_temp_template.dlabel.nii COLUMN ADAP_BARY_AREA CUBIC ${ResultsFolder}/${NameOffMRI}_temp_atlas.dtseries.nii -volume-predilate 30
+    ${CARET7DIR}/wb_command -cifti-resample ${ResultsFolder}/${NameOffMRI}_temp_subject_dilate.dtseries.nii COLUMN ${ResultsFolder}/${NameOffMRI}_temp_template.dlabel.nii COLUMN ADAP_BARY_AREA CUBIC ${ResultsFolder}/${NameOffMRI}_temp_atlas.dtseries.nii -volume-predilate 10
 fi
 
 #delete common temporaries
 rm -f ${ResultsFolder}/${NameOffMRI}_temp_subject_dilate.dtseries.nii
 rm -f ${ResultsFolder}/${NameOffMRI}_temp_template.dlabel.nii
 
+#the standard space output cifti must not contain zeros (or correlation, ICA, variance normalization, etc will break), so dilate in case freesurfer was unable to segment something (may only be applicable for bad quality structurals)
+#NOTE: wb_command v1.4.0 and later should only output exact 0s past the edge of predilate, so this works as desired
+#earlier verions of wb_command may produce undesired results in the subjects that need this dilation
+${CARET7DIR}/wb_command -cifti-dilate ${ResultsFolder}/${NameOffMRI}_temp_atlas.dtseries.nii COLUMN 0 30 ${ResultsFolder}/${NameOffMRI}_temp_atlas_dilate.dtseries.nii
+rm -f ${ResultsFolder}/${NameOffMRI}_temp_atlas.dtseries.nii
+
 #write output volume, delete temporary
 #NOTE: $VolumefMRI contains a path in it, it is not a file in the current directory
-${CARET7DIR}/wb_command -cifti-separate ${ResultsFolder}/${NameOffMRI}_temp_atlas.dtseries.nii COLUMN -volume-all "$VolumefMRI"_AtlasSubcortical_s"$SmoothingFWHM".nii.gz
-rm -f ${ResultsFolder}/${NameOffMRI}_temp_atlas.dtseries.nii
+${CARET7DIR}/wb_command -cifti-separate ${ResultsFolder}/${NameOffMRI}_temp_atlas_dilate.dtseries.nii COLUMN -volume-all "$VolumefMRI"_AtlasSubcortical_s"$SmoothingFWHM".nii.gz
+rm -f ${ResultsFolder}/${NameOffMRI}_temp_atlas_dilate.dtseries.nii
 
 log_Msg "END"
 
