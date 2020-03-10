ef875288dd3e0458a6f38074bd95818db64e3664
Matt Glasser <glasserm@wusm.wustl.edu>
2020-03-10 18:07:29 -0500

    Update icaDim.m
    Try again now with max 4 copies.

--- ICAFIX/scripts/icaDim.m
+++ ICAFIX/scripts/icaDim.m
@@ -44,15 +44,18 @@ end
 [u,EigS,v]=nets_svds(data_detrend',0); %Eigenvalues of detrended data
 Out.DOF=sum(diag(EigS)>(std(diag(EigS))*0.1)); %Compute degrees of freedom
 
+data_detrend_vn=0; %To allow clearing later in loop
+
 c=1;
 stabCount = 0;
 while stabCount < stabThresh %Loop until dim output is stable
         
     c
+    clear data_detrend_vn;
     %Variance normalization via PCA reconstruction: Isolate unstructured noise
     if VN~=0
       noise_unst = (u(:,Out.VNDIM(c):Out.DOF)*EigS(Out.VNDIM(c):Out.DOF,Out.VNDIM(c):Out.DOF)*v(:,Out.VNDIM(c):Out.DOF)')';
-      Out.noise_unst_std = max(std(noise_unst,[],2),0.001);
+      Out.noise_unst_std = max(std(noise_unst,[],2),0.001); clear noise_unst;
       data_detrend_vn = data_detrend ./ repmat(Out.noise_unst_std,1,size(data_detrend,2));
     elseif VN==0
       data_detrend_vn = data_detrend;
@@ -186,7 +189,13 @@ if DEMDT~=-1
     [u,EigS,v]=nets_svds(demean(data_detrend_vn)',0);
 else
     [u,EigS,v]=nets_svds(data_detrend_vn',0);
-end    
+end
+
+mean_data_detrend_vn=mean(data_detrend_vn);
+size_data_detrend_vnOne=size(data_detrend_vn,1);
+size_data_detrend_vnTwo=size(data_detrend_vn,2);
+clear data_detrend_vn;
+
 u(isnan(u))=0; v(isnan(v))=0;
 Out.EigSAdj=single(zeros(length(EigS),1));
 Out.grot_one=diag(EigS(1:length(Out.EN),1:length(Out.EN)));
@@ -228,9 +237,9 @@ Out.EigSAdj(1:length(Out.EN))=sqrt(Out.grot_six);
 
 Out.data=single(zeros(OrigdataSizeOne,OrigdataSizeTwo));
 if DEMDT~=-1
-    Out.data(OrigdataMask,:)=data_trend + (((u*diag(Out.EigSAdj)*v')'+repmat(mean(data_detrend_vn),size(data_detrend_vn,1),1)) .* repmat(Out.noise_unst_std,1,size(data_detrend_vn,2)));
+    Out.data(OrigdataMask,:)=data_trend + (((u*diag(Out.EigSAdj)*v')'+repmat(mean_data_detrend_vn,size_data_detrend_vnOne,1)) .* repmat(Out.noise_unst_std,1,size_data_detrend_vnTwo));
 else
-    Out.data(OrigdataMask,:)=data_trend + (((u*diag(Out.EigSAdj)*v')') .* repmat(Out.noise_unst_std,1,size(data_detrend_vn,2)));
+    Out.data(OrigdataMask,:)=data_trend + (((u*diag(Out.EigSAdj)*v')') .* repmat(Out.noise_unst_std,1,size_data_detrend_vnTwo));
 end
 temp=single(zeros(OrigdataSizeOne,1)); temp(OrigdataMask,:)=Out.noise_unst_std; Out.noise_unst_std=max(temp,0.001); clear temp;
 
