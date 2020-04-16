795d0aff013437b5271fa73c3a26278e916d6bea
Tim Coalson <tsc5yc@mst.edu>
2020-04-16 16:17:48 -0500

    fix matlab path precedence in RestingStateStats to match compilation

--- RestingStateStats/RestingStateStats.sh
+++ RestingStateStats/RestingStateStats.sh
@@ -968,7 +968,7 @@ main()
 			mGlobalPath="${HCPPIPEDIR}/global/matlab"
 			mFslPath="${FSLDIR}/etc/matlab"
 
-			matlabCode="addpath '$HCPCIFTIRWDIR'; addpath '$mGlobalPath'; addpath '$mPath'; addpath '$mFslPath'; RestingStateStats('${motionparameters}',${g_high_pass},${TR},'${ICAs}','${noise}','${CARET7DIR}/wb_command','${dtseries}','${bias}','${RssPrefix}','${g_dlabel_file}','${g_bc_mode}','${g_out_string}','${WM}','${CSF}');"
+			matlabCode="addpath '$mFslPath'; addpath '$HCPCIFTIRWDIR'; addpath '$mGlobalPath'; addpath '$mPath'; RestingStateStats('${motionparameters}',${g_high_pass},${TR},'${ICAs}','${noise}','${CARET7DIR}/wb_command','${dtseries}','${bias}','${RssPrefix}','${g_dlabel_file}','${g_bc_mode}','${g_out_string}','${WM}','${CSF}');"
 
 			log_Msg "Run interpreted MATLAB/Octave (${interpreter[@]}) with command..."
 			log_Msg "$matlabCode"
