5719c91af6698445b0cc632830e37aef044a4c32
Takuya Hayashi <takuya.hayashi@gmail.com>
2023-03-01 08:51:57 +0900

    Use "world" mm space for aff2rigid (#245)
    Allows it to generically align the AC for any template where the AC is the volume origin (such as NHP)

--- PreFreeSurfer/scripts/ACPCAlignment.sh
+++ PreFreeSurfer/scripts/ACPCAlignment.sh
@@ -129,7 +129,9 @@ ${FSLDIR}/bin/convert_xfm -omat "$WD"/full2std.mat -concat "$WD"/roi2std.mat "$W
 
 # Get a 6 DOF approximation which does the ACPC alignment (AC, ACPC line, and hemispheric plane)
 verbose_echo " --> Geting a 6 DOF approximation"
-${FSLDIR}/bin/aff2rigid "$WD"/full2std.mat "$OutputMatrix"
+$CARET7DIR/wb_command -convert-affine -from-flirt "$WD"/full2std.mat "$Input".nii.gz "$Reference" -to-world "$WD"/full2std_world.mat
+${HCPPIPEDIR}/global/scripts/aff2rigid_world "$WD"/full2std_world.mat "$WD"/full2std_rigid_world.mat
+$CARET7DIR/wb_command -convert-affine -from-world "$WD"/full2std_rigid_world.mat -to-flirt "$OutputMatrix" "$Input".nii.gz "$Reference"
 
 # Create a resampled image (ACPC aligned) using spline interpolation
 verbose_echo " --> Creating a resampled image"
