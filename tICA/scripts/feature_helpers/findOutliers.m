05faf39eba7800f005be08c37f18db0714ffa208
cyang31 <alexyang9631@gmail.com>
2023-04-25 17:24:02 -0500

    address comment

--- tICA/scripts/feature_helpers/findOutliers.m
+++ tICA/scripts/feature_helpers/findOutliers.m
@@ -1,5 +1,6 @@
 function ret = findOutliers(data)
 % must be 1d array
+% outlier detection: identify data that do not pass the MAD-based test, while ensuring that it never detects more than 5% of the highest or lowest values.
 madOutliers=findScaledMADOutliers(data);
 perOutliers=findFivePercentageOutliers(data);
 ret=and(madOutliers, perOutliers);
