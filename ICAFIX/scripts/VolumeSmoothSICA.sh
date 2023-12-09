5800cb31089a21b8c4781ca080beab85a21f9e46
Tim Coalson <coalsont@users.noreply.github.com>
2023-12-08 18:22:59 -0600

    fix fwhm math formatting

--- ICAFIX/scripts/VolumeSmoothSICA.sh
+++ ICAFIX/scripts/VolumeSmoothSICA.sh
@@ -6,7 +6,7 @@ Resolution="${5}"
 SmoothingFWHM="${6}"
 Caret7_command="${7}"
 
-SmoothingSigma=`echo "$SmoothingFWHM / ( 2 * ( sqrt ( 2 * l ( 2 ) ) ) )" | bc -l`
+SmoothingSigma=`echo "$SmoothingFWHM / (2 * sqrt(2 * l(2)))" | bc -l`
 
 ${Caret7_command} -volume-label-import ${SubjectFolder}/MNINonLinear/ROIs/VolumeSmoothROIs.${Resolution}.nii.gz "" ${SubjectFolder}/MNINonLinear/ROIs/VolumeSmoothROIs.${Resolution}.nii.gz
 ${Caret7_command} -volume-parcel-smoothing ${SubjectFolder}/MNINonLinear/Results/${fMRIName}/${fMRIName}_hp${HighPass}.ica/filtered_func_data.ica/melodic_oIC.nii.gz ${SubjectFolder}/MNINonLinear/ROIs/VolumeSmoothROIs.${Resolution}.nii.gz ${SmoothingSigma} ${SubjectFolder}/MNINonLinear/Results/${fMRIName}/${fMRIName}_hp${HighPass}.ica/filtered_func_data.ica/melodic_oIC_s4.nii.gz
