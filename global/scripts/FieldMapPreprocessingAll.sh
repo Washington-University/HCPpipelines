50eb16528156143b62d4bed3cf88f43c72a805bd
Brice Fernandez <62889889+brice82@users.noreply.github.com>
2024-02-19 12:11:03 +0100

    Update global/scripts/FieldMapPreprocessingAll.sh
    Co-authored-by: Michael Harms <mharms@wustl.edu>

--- global/scripts/FieldMapPreprocessingAll.sh
+++ global/scripts/FieldMapPreprocessingAll.sh
@@ -46,7 +46,7 @@ opts_AddMandatory '--method' 'DistortionCorrection' 'method' "method to use for 
         '${SIEMENS_METHOD_OPT}'
              use Siemens specific Gradient Echo Field Maps for SDC
         '${GENERAL_ELECTRIC_METHOD_OPT}'
-             use GE HealthCare Legacy specific Gradient Echo Field Maps for SDC (fieldmap and magnitude in a single NIfTI file via --GEB0InputName argument).
+             use GE HealthCare Legacy specific Gradient Echo Field Maps for SDC (field map in Hz and magnitude image in a single NIfTI file, via --GEB0InputName argument).
         '${GE_HEALTHCARE_METHOD_OPT}'
              use GE HealthCare specific Gradient Echo Field Maps for SDC (fieldmap and magnitude in two separate NIfTI files).
         '${PHILIPS_METHOD_OPT}'
