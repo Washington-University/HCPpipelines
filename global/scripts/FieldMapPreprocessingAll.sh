a9a24ed3b80b806132b1f18ad0f18ce8c1d066c0
Brice Fernandez <62889889+brice82@users.noreply.github.com>
2024-02-19 12:09:49 +0100

    Update global/scripts/FieldMapPreprocessingAll.sh
    Co-authored-by: Michael Harms <mharms@wustl.edu>

--- global/scripts/FieldMapPreprocessingAll.sh
+++ global/scripts/FieldMapPreprocessingAll.sh
@@ -18,7 +18,7 @@ GENERAL_ELECTRIC_METHOD_OPT="GEHealthCareLegacyFieldMap"
 GE_HEALTHCARE_METHOD_OPT="GEHealthCareFieldMap"
 # "GEHealthCareFieldMap" refers to fieldmap in the form of 2 separate NIfTI files.
 # One file with the Fieldmap in Hz and another file with the magnitude image). 
-# Note: dcm2niix (v1.0.20210410 and later) convert the GEHC B0Maps as 2 NIFTI files
+# Note: dcm2niix (v1.0.20210410 and later) convert the GEHC B0Maps as 2 separate NIFTI files
 # using the suffix _fieldmaphz for the fieldmap in Hertz and no suffix for the magnitude. 
 
 PHILIPS_METHOD_OPT="PhilipsFieldMap"
