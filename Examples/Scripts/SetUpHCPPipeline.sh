8844dded013984e172bcea8d52ca7f7adac435ac
Burke Rosen <bqrosen@wustl.edu>
2024-10-23 13:16:10 -0500

    put melodicicasso back in. commented out FSL_FIXDIR in setup, added --enable-legacy-fix to hcp_fix_multi_run. now checks if --enable-legacy-fix set to true (defualt) and if /fix exists and is legacy fix. If all these are true, runs legacy fix, otherwise runs pyfix.

--- Examples/Scripts/SetUpHCPPipeline.sh
+++ Examples/Scripts/SetUpHCPPipeline.sh
@@ -22,7 +22,7 @@ fi
 ## Edit this section: set up other environment variables
 export MSMBINDIR="${HOME}/pipeline_tools/MSM"
 export MATLAB_COMPILER_RUNTIME=/export/matlab/MCR/R2017b/v93
-export FSL_FIXDIR=/usr/local/fix
+# export FSL_FIXDIR=/usr/local/fix # only needed for legacy fix
 # If a suitable version of wb_command is on your $PATH, CARET7DIR can be blank
 export CARET7DIR=
 export HCPCIFTIRWDIR="$HCPPIPEDIR"/global/matlab/cifti-matlab
