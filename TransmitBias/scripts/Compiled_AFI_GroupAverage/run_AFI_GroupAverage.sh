c83db465f6da64c32a4056d2170fb8042f0355bc
Tim Coalson <coalsont@wustl.edu>
2024-11-12 16:14:22 -0600

    test recompilation with MCR2022b

--- TransmitBias/scripts/Compiled_AFI_GroupAverage/run_AFI_GroupAverage.sh
+++ TransmitBias/scripts/Compiled_AFI_GroupAverage/run_AFI_GroupAverage.sh
@@ -20,6 +20,9 @@ else
   LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCRROOT}/sys/opengl/lib/glnxa64;
   export LD_LIBRARY_PATH;
   echo LD_LIBRARY_PATH is ${LD_LIBRARY_PATH};
+# Preload glibc_shim in case of RHEL7 variants
+  test -e /usr/bin/ldd &&  ldd --version |  grep -q "(GNU libc) 2\.17"  \
+            && export LD_PRELOAD="${MCRROOT}/bin/glnxa64/glibc-2.17_shim.so"
   shift 1
   args=
   while [ $# -gt 0 ]; do
