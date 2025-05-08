b3e3905b7164772ffc69dd480a5439439287e43d
Tim Coalson <coalsont@wustl.edu>
2025-05-07 22:57:22 -0500

    Conflicts: 	PreFreeSurfer/PreFreeSurferPipeline.sh 	PreFreeSurfer/scripts/ACPCAlignment.sh 	PreFreeSurfer/scripts/AnatomicalAverage.sh 	PreFreeSurfer/scripts/AtlasRegistrationToMNI152_FLIRTandFNIRT.sh 	PreFreeSurfer/scripts/BiasFieldCorrection_T1wOnly.sh 	PreFreeSurfer/scripts/BiasFieldCorrection_sqrtT1wXT2w.sh 	PreFreeSurfer/scripts/BrainExtraction_FNIRTbased.sh 	PreFreeSurfer/scripts/T2wToT1wDistortionCorrectAndReg.sh 	global/scripts/TopupPreprocessingAll.sh

--- PreFreeSurfer/scripts/AnatomicalAverage.sh
+++ PreFreeSurfer/scripts/AnatomicalAverage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+<<<<<<< HEAD
 
 # ------------------------------------------------------------------------------
 #  Usage Description Function
@@ -40,6 +41,29 @@ opts_ParseArguments "$@"
 
 #display the parsed/default values
 opts_ShowValues
+=======
+set -e
+
+Usage() {
+    echo ""
+    echo "Usage: `basename $0` [options] <image1> ... <imageN>"
+    echo ""
+    echo "Compulsory arguments"
+    echo "  -o <name>        : output basename"
+    echo "Optional arguments"
+    echo "  -s <image>       : standard image (e.g. MNI152_T1_2mm)"
+    echo "  -m <image>       : standard brain mask (e.g. MNI152_T1_2mm_brain_mask_dil)"
+    echo "  -n               : do not crop images"
+    echo "  -w <dir>         : local, temporary working directory (to be cleaned up - i.e. deleted)"
+    echo "  --noclean        : do not run the cleanup"
+    echo "  -v               : verbose output"
+    echo "  -h               : display this help message"
+    echo ""
+    echo "e.g.:  `basename $0` -n -o output_name  im1 im2"
+    echo "       Note that N>=2 (i.e. there must be at least two images in the list)"
+    exit 1
+}
+>>>>>>> RIKEN/fix/PreFreeSurferPipeline
 
 if ((pipedirguessed))
 then
@@ -48,15 +72,82 @@ fi
 
 log_Check_Env_Var FSLDIR
 
+<<<<<<< HEAD
 crop=$(opts_StringToBool "$crop")
 cleanup=$(opts_StringToBool "$cleanup")
+=======
+# deal with options
+crop=yes
+verbose=no
+wdir=
+cleanup=yes
+StandardImage=$FSLDIR/data/standard/MNI152_T1_2mm.nii.gz
+StandardMask=$FSLDIR/data/standard/MNI152_T1_2mm_brain_mask_dil.nii.gz
+
+if [ $# -eq 0 ] ; then Usage; exit 0; fi
+while [ $# -ge 1 ] ; do
+    iarg=$1
+    case "$iarg"
+	in
+	-n)
+	    crop=no; 
+	    shift;;
+	-o)
+	    output=`get_arg2 $1 $2`;
+	    shift 2;;
+	-s)
+	    StandardImage=`get_arg2 $1 $2`;
+	    shift 2;;
+	-m)
+	    StandardMask=`get_arg2 $1 $2`;
+	    shift 2;;
+	-w)
+	    wdir=`get_arg2 $1 $2`;
+	    cleanup=no;
+	    shift 2;;
+	-b)
+	    BrainSizeOpt=`get_arg2 $1 $2`;
+	    BrainSizeOpt="-b $BrainSizeOpt";
+	    shift 2;;
+	-v)
+	    verbose=yes; 
+	    shift;;
+	-h)
+	    Usage;
+	    exit 0;;
+	--noclean)
+	    cleanup=no;
+	    shift;;
+	*)
+	    if [ `echo $1 | sed 's/^\(.\).*/\1/'` = "-" ] ; then 
+		echo "Unrecognised option $1" 1>&2
+		exit 1
+	    fi
+	    imagelist="$imagelist $1"
+	    shift;;
+    esac
+done
+>>>>>>> RIKEN/fix/PreFreeSurferPipeline
 
 #########################################################################################################
 
+<<<<<<< HEAD
 IFS='@' read -a imagelist <<<"$imagesStr"
 
 if ((${#imagelist[@]} < 2)) ; then
     log_Err_Abort "Must specify at least two images to average"
+=======
+if [ X$output = X ] ; then
+  echo "The compulsory argument -o MUST be used"
+  exit 1;
+fi
+
+if [ `echo $imagelist | wc -w` -lt 2 ] ; then
+  Usage;
+  echo " "
+  echo "Must specify at least two images to average"
+  exit 1;
+>>>>>>> RIKEN/fix/PreFreeSurferPipeline
 fi
 
 # setup working directory
@@ -66,7 +157,8 @@ if [ X$wdir = X ] ; then
 fi
 if [ ! -d $wdir ] ; then
     if [ -f $wdir ] ; then 
-		log_Err_Abort "A file already exists with the name $wdir - cannot use this as the working directory"
+	echo "A file already exists with the name $wdir - cannot use this as the working directory"
+	exit 1;
     fi
     mkdir $wdir
 fi
@@ -80,6 +172,11 @@ for fn in "${imagelist[@]}" ; do
     newimlist="$newimlist $wdir/$bnm"
 done
 
+<<<<<<< HEAD
+=======
+if [ $verbose = yes ] ; then echo "Images: $imagelist  Output: $output"; fi
+
+>>>>>>> RIKEN/fix/PreFreeSurferPipeline
 # for each image reorient, register to std space, (optionally do "get transformed FOV and crop it based on this")
 for fn in $newimlist ; do
     $FSLDIR/bin/fslreorient2std ${fn}.nii.gz ${fn}_reorient
@@ -99,16 +196,20 @@ im1=`echo $newimlist | awk '{ print $1 }'`;
 for im2 in $newimlist ; do
     if [ $im2 != $im1 ] ; then
         # register version of two images (whole heads still)
-		$FSLDIR/bin/flirt -in ${im2}_roi -ref ${im1}_roi -omat ${im2}_to_im1.mat -out ${im2}_to_im1 -dof 6 -searchrx -30 30 -searchry -30 30 -searchrz -30 30 
-		
+	$FSLDIR/bin/flirt -in ${im2}_roi -ref ${im1}_roi -omat ${im2}_to_im1.mat -out ${im2}_to_im1 -dof 6 -searchrx -30 30 -searchry -30 30 -searchrz -30 30 
+
         # transform std space brain mask
-		$FSLDIR/bin/flirt -init ${im1}_std2roi.mat -in "$StandardMask" -ref ${im1}_roi -out ${im1}_roi_linmask -applyxfm
-		
+	$FSLDIR/bin/flirt -init ${im1}_std2roi.mat -in "$StandardMask" -ref ${im1}_roi -out ${im1}_roi_linmask -applyxfm
+
         # re-register using the brain mask as a weighting image
+<<<<<<< HEAD
         #TSC: "-nosearch" only sets angle to 0, still optimizes translation
 		$FSLDIR/bin/flirt -in ${im2}_roi -init ${im2}_to_im1.mat -omat ${im2}_to_im1_linmask.mat -out ${im2}_to_im1_linmask -ref ${im1}_roi -refweight ${im1}_roi_linmask -nosearch
+=======
+	$FSLDIR/bin/flirt -in ${im2}_roi -init ${im2}_to_im1.mat -omat ${im2}_to_im1_linmask.mat -out ${im2}_to_im1_linmask -ref ${im1}_roi -refweight ${im1}_roi_linmask -nosearch
+>>>>>>> RIKEN/fix/PreFreeSurferPipeline
     else
-		cp $FSLDIR/etc/flirtsch/ident.mat ${im1}_to_im1_linmask.mat
+	cp $FSLDIR/etc/flirtsch/ident.mat ${im1}_to_im1_linmask.mat
     fi
 done
 
