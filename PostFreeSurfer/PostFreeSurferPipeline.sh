92101e709160e658e056321145a428d8910e0423
Nagehan Demirci <51086894+nagedem@users.noreply.github.com>
2025-09-30 11:37:17 -0500

    Update PostFreeSurfer/PostFreeSurferPipeline.sh
    Co-authored-by: Tim Coalson <coalsont@users.noreply.github.com>

--- PostFreeSurfer/PostFreeSurferPipeline.sh
+++ PostFreeSurfer/PostFreeSurferPipeline.sh
@@ -90,7 +90,10 @@ opts_AddOptional '--inflatescale' 'InflateExtraScale' 'number' "surface inflatio
 opts_AddOptional '--processing-mode' 'ProcessingMode' 'HCPStyleData|LegacyStyleData' "disable some HCP preprocessing requirements to allow processing of data that doesn't meet HCP acquisition guidelines - don't use this if you don't need to" 'HCPStyleData'
 opts_AddOptional '--structural-qc' 'QCMode' 'yes|no|only' "whether to run structural QC, default 'yes'" 'yes'
 opts_AddOptional '--use-ind-mean' 'UseIndMean' 'YES or NO' "whether to use the mean of the session's myelin map as reference map's myelin map mean, defaults to 'YES'" 'YES'
-opts_AddOptional '--metric-regression' 'MetricReg' 'OLD, NEW or BOTH' "whether to use the updated curvature-thickness regression, defaults to 'BOTH'" 'BOTH'
+opts_AddOptional '--metric-regression' 'MetricReg' 'OLD, NEW or BOTH' "whether to use the updated curvature-thickness regression, defaults to 'BOTH'
+NOTE: if you run with 'OLD' and later decide you want the results from the new method, it is NOT RECOMMENDED to rerun PostFreeSurferPipeline.sh again, because it runs a registration that shouldn't be changed after other preprocessing pipelines are run.
+Instead, run global/scripts/CorrThick.sh with appropriate arguments, which will only generate the new folding-compensated thickness outputs.
+'NEW' or 'BOTH' require python 3 with numpy, nibabel, and scipy installed." 'BOTH'
 
 opts_AddOptional '--subject-long' 'Subject' 'subject ID' "subject label (used in longitudinal mode), may be different from Session"
 opts_AddOptional '--longitudinal-mode' 'LongitudinalMode' 'NONE|TIMEPOINT_STAGE1|TIMEPOINT_STAGE2|TEMPLATE' "longitudinal processing mode
