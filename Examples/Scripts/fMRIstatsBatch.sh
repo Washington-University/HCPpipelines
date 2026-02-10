47ae3e25c3b157e144d3492a6ac310d759b026ec
Burke Rosen <bqrosen@wustl.edu>
2026-02-10 16:40:16 -0600

    remove main function from batch script

--- Examples/Scripts/fMRIstatsBatch.sh
+++ Examples/Scripts/fMRIstatsBatch.sh
@@ -115,109 +115,101 @@ get_options() {
 #   Gets user specified command line options and runs fMRIStats on multiple subjects
 #   to compute quality metrics on ICA+FIX cleaned data
 #
-main() {
 
-    # get command line options
-    get_options "$@"
+# get command line options
+get_options "$@"
 
-    # set up pipeline environment variables and software
-    source "${EnvironmentScript}"
+# set up pipeline environment variables and software
+source "${EnvironmentScript}"
 
-    # general settings
-    # set list of fMRI runs on which ICA+FIX has been run, use @ to separate multiple
-    ConcatNames="tfMRI_Concat"
+# general settings
+# set list of fMRI runs on which ICA+FIX has been run, use @ to separate multiple
+ConcatNames="tfMRI_Concat"
 
-    # set the file name component representing the preprocessing already done, e.g. '_clean'
-    fMRIProcSTRING="_clean_rclean_tclean"
+# set the file name component representing the preprocessing already done, e.g. '_clean'
+fMRIProcSTRING="_clean_rclean_tclean"
 
-    # set temporal highpass full-width (2*sigma) used in ICA+FIX, should match with $fMRIProcSTRING
-    HighPass="0"
+# set temporal highpass full-width (2*sigma) used in ICA+FIX, should match with $fMRIProcSTRING
+HighPass="0"
 
-    # set whether to process volume data in addition to surface data
-    ProcessVolume="TRUE"
+# set whether to process volume data in addition to surface data
+ProcessVolume="TRUE"
 
-    # set whether to compute cleanup effects metrics (comparing cleaned vs uncleaned data)
-    CleanUpEffects="TRUE"
+# set whether to compute cleanup effects metrics (comparing cleaned vs uncleaned data)
+CleanUpEffects="TRUE"
 
-    # tICA mode
-    ICAmode="sICA+tICA" # options: 'sICA' or 'sICA+tICA'
-    # 
-    # sICATCS and Signal are always required (auto-constructed from standard paths):
-    #   - sICATCS: {fMRIFolder}/{ConcatName}_hp{HighPass}.ica/filtered_func_data.ica/melodic_mix.sdseries.nii
-    #   - Signal: {fMRIFolder}/{ConcatName}_hp{HighPass}.ica/HandSignal.txt or Signal.txt
-    #
-    # If ICAmode="sICA+tICA", you can also provide:
-    #   - tICAcomponentTCS: @ delimited list one path per subject
-    #   - tICAcomponentNoise: single group file 
-    # These files path are not automatically constructed because their names and locations are not necessarily programmatically derivable 
-    tICAcomponentTCS="${StudyFolder}/100307/MNINonLinear/fsaverage_LR32k/100307.tfMRI_Concat_d72_WF6_S1200_MSMAll3T475T_WR_tICA_MSMAll_ts.32k_fs_LR.sdseries.nii" # path to tICA timecourse CIFTI (@ delimited)
-    tICAcomponentNoise="${StudyFolder}/S1200_MSMAll3T1071/MNINonLinear/Results/tfMRI_Concat/Pre_tICA/tICA_d72/Noise.txt" # path to tICA component noise indices text file (same for all subjects)
+# tICA mode
+ICAmode="sICA+tICA" # options: 'sICA' or 'sICA+tICA'
+# 
+# sICATCS and Signal are always required (auto-constructed from standard paths):
+#   - sICATCS: {fMRIFolder}/{ConcatName}_hp{HighPass}.ica/filtered_func_data.ica/melodic_mix.sdseries.nii
+#   - Signal: {fMRIFolder}/{ConcatName}_hp{HighPass}.ica/HandSignal.txt or Signal.txt
+#
+# If ICAmode="sICA+tICA", you can also provide:
+#   - tICAcomponentTCS: @ delimited list one path per subject
+#   - tICAcomponentNoise: single group file 
+# These files path are not automatically constructed because their names and locations are not necessarily programmatically derivable 
+tICAcomponentTCS="${StudyFolder}/100307/MNINonLinear/fsaverage_LR32k/100307.tfMRI_Concat_d72_WF6_S1200_MSMAll3T475T_WR_tICA_MSMAll_ts.32k_fs_LR.sdseries.nii" # path to tICA timecourse CIFTI (@ delimited)
+tICAcomponentNoise="${StudyFolder}/S1200_MSMAll3T1071/MNINonLinear/Results/tfMRI_Concat/Pre_tICA/tICA_d72/Noise.txt" # path to tICA component noise indices text file (same for all subjects)
 
-    # end of general inputs
+# end of general inputs
 
-    # set registration string
-    if [ "${RegName}" != "NONE" ] ; then
-        RegString="_${RegName}"
-    else
-        RegString=""
-    fi
+# set registration string
+if [ "${RegName}" != "NONE" ] ; then
+    RegString="_${RegName}"
+else
+    RegString=""
+fi
 
-    # Convert @ separated lists to arrays
-    IFS='@' read -ra SubjectArray <<< "$Subjlist"
-    IFS='@' read -ra ConcatNamesArray <<< "$ConcatNames"
-    IFS='@' read -ra tICAcomponentTCSArray <<< "$tICAcomponentTCS"
+# Convert @ separated lists to arrays
+IFS='@' read -ra SubjectArray <<< "$Subjlist"
+IFS='@' read -ra ConcatNamesArray <<< "$ConcatNames"
+IFS='@' read -ra tICAcomponentTCSArray <<< "$tICAcomponentTCS"
 
-    # Loop through subjects and queue parallel jobs
-    for ((subjectIndex = 0; subjectIndex < ${#SubjectArray[@]}; ++subjectIndex))
+# Loop through subjects and queue parallel jobs
+for ((subjectIndex = 0; subjectIndex < ${#SubjectArray[@]}; ++subjectIndex))
+do
+    Subject="${SubjectArray[$subjectIndex]}"
+    # Build list of fMRI files that exist for this subject
+    fMRIExist=()
+    for ConcatName in "${ConcatNamesArray[@]}"
     do
-        Subject="${SubjectArray[$subjectIndex]}"
-        # Build list of fMRI files that exist for this subject
-        fMRIExist=()
-        for ConcatName in "${ConcatNamesArray[@]}"
-        do
-            if [[ -f "${StudyFolder}/${Subject}/MNINonLinear/Results/${ConcatName}/${ConcatName}_Atlas${RegString}_hp${HighPass}${fMRIProcSTRING}.dtseries.nii" ]]
-            then
-                fMRIExist+=("${ConcatName}")
-            fi
-        done
-        
-        # Convert array to @ separated string for passing to fMRIStats
-        ConcatNamesForSub=$(IFS='@'; echo "${fMRIExist[*]}")
-        
-        # Only queue job if subject has data
-        if [[ "$ConcatNamesForSub" != "" ]]
+        if [[ -f "${StudyFolder}/${Subject}/MNINonLinear/Results/${ConcatName}/${ConcatName}_Atlas${RegString}_hp${HighPass}${fMRIProcSTRING}.dtseries.nii" ]]
         then
-            if ((RunLocal)) || [[ "$QUEUE" == "" ]]
-            then
-                echo "running locally"
-                queuing_command=("$HCPPIPEDIR"/global/scripts/captureoutput.sh)
-            else
-                echo "queueing with fsl_sub to to $QUEUE"
-                queuing_command=("$FSLDIR/bin/fsl_sub" -q "$QUEUE")
-            fi
-
-            # fMRIStats pipeline
-            "${queuing_command[@]}" "$HCPPIPEDIR"/fMRIStats/fMRIStats.sh \
-                --study-folder="$StudyFolder" \
-                --subject="$Subject" \
-                --concat-names="$ConcatNamesForSub" \
-                --high-pass="$HighPass" \
-                --proc-string="$fMRIProcSTRING" \
-                --reg-name="$RegName" \
-                --process-volume="$ProcessVolume" \
-                --cleanup-effects="$CleanUpEffects" \
-                --ica-mode="$ICAmode" \
-                --tica-component-tcs="${tICAcomponentTCSArray[$subjectIndex]}" \
-                --tica-component-noise="$tICAcomponentNoise" \
-                --matlab-run-mode="$MatlabMode"
-        else
-            echo "Skipping ${Subject}: no runs with cleaned data found"
+            fMRIExist+=("${ConcatName}")
         fi
     done
     
-}
+    # Convert array to @ separated string for passing to fMRIStats
+    ConcatNamesForSub=$(IFS='@'; echo "${fMRIExist[*]}")
+    
+    # Only queue job if subject has data
+    if [[ "$ConcatNamesForSub" != "" ]]
+    then
+        if ((RunLocal)) || [[ "$QUEUE" == "" ]]
+        then
+            echo "running locally"
+            queuing_command=("$HCPPIPEDIR"/global/scripts/captureoutput.sh)
+        else
+            echo "queueing with fsl_sub to to $QUEUE"
+            queuing_command=("$FSLDIR/bin/fsl_sub" -q "$QUEUE")
+        fi
 
-#
-# Invoke the main function to get things started
-#
-main "$@"
+        # fMRIStats pipeline
+        "${queuing_command[@]}" "$HCPPIPEDIR"/fMRIStats/fMRIStats.sh \
+            --study-folder="$StudyFolder" \
+            --subject="$Subject" \
+            --concat-names="$ConcatNamesForSub" \
+            --high-pass="$HighPass" \
+            --proc-string="$fMRIProcSTRING" \
+            --reg-name="$RegName" \
+            --process-volume="$ProcessVolume" \
+            --cleanup-effects="$CleanUpEffects" \
+            --ica-mode="$ICAmode" \
+            --tica-component-tcs="${tICAcomponentTCSArray[$subjectIndex]}" \
+            --tica-component-noise="$tICAcomponentNoise" \
+            --matlab-run-mode="$MatlabMode"
+    else
+        echo "Skipping ${Subject}: no runs with cleaned data found"
+    fi
+done
