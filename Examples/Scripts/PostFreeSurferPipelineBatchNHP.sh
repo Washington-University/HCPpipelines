523bad784d2ac83b3a51bf05de8900728cfe78f7
Azure2003 <brainmappers@brainmappers-desktop2.wustl.edu>
2026-02-16 10:37:41 -0600

    +addressing reviewer comments https://github.com/Washington-University/HCPpipelines/pull/366/changes#r2795602065 https://github.com/Washington-University/HCPpipelines/pull/366/changes#r2795603039

--- Examples/Scripts/PostFreeSurferPipelineBatchNHP.sh
+++ Examples/Scripts/PostFreeSurferPipelineBatchNHP.sh
@@ -117,6 +117,12 @@ for Session in $Sesslist ; do
     #GrayordinatesResolutions="2" #Usually 2mm, if multiple delimit with @, must already exist in templates dir
     HighResMesh="164" #Usually 164k vertices
     #LowResMeshes="32" #Usually 32k vertices, if multiple delimit with @, must already exist in templates dir
+
+    # These are values for humans from SetUpSpecies.sh:
+    # CorrectionSigma=5
+    # MyelinMappingFWHM=4
+    # SurfaceSmoothingFWHM=7
+
     SubcorticalGrayLabels="${HCPPIPEDIR_Config}/FreeSurferSubcorticalLabelTableLut.txt"
     FreeSurferLabels="${HCPPIPEDIR_Config}/FreeSurferAllLut.txt"
     ReferenceMyelinMaps="${HCPPIPEDIR_Templates}/standard_mesh_atlases/Conte69.MyelinMap_BC.164k_fs_LR.dscalar.nii"
@@ -153,19 +159,22 @@ for Session in $Sesslist ; do
 
     # The following lines are used for interactive debugging to set the positional parameters: $1 $2 $3 ...
 
-    echo "set -- --study-folder=$StudyFolder \
-        --session=$Session \
-        --surfatlasdir=$SurfaceAtlasDIR \
-        --grayordinatesdir=$GrayordinatesSpaceDIR \
+    echo "set -- --study-folder=$StudyFolder         \
+        --session=$Session                           \
+        --surfatlasdir=$SurfaceAtlasDIR              \
+        --grayordinatesdir=$GrayordinatesSpaceDIR    \
         --grayordinatesres=$GrayordinatesResolutions \
-        --hiresmesh=$HighResMesh \
-        --lowresmesh=$LowResMeshes \
-        --subcortgraylabels=$SubcorticalGrayLabels \
-        --freesurferlabels=$FreeSurferLabels \
-        --refmyelinmaps=$ReferenceMyelinMaps \
-        --regname=$RegName \
-        --use-ind-mean="$UseIndMean"" \
-        --species="$Species"
+        --hiresmesh=$HighResMesh                     \
+        --lowresmesh=$LowResMeshes                   \
+        --subcortgraylabels=$SubcorticalGrayLabels   \
+        --freesurferlabels=$FreeSurferLabels         \
+        --refmyelinmaps=$ReferenceMyelinMaps         \
+        --regname=$RegName                           \ 
+        --use-ind-mean="$UseIndMean""                \
+        --species="$Species"                         \
+        --mcsigma="$CorrectionSigma"                 \
+        --myelin-voume-fwhm="$MyelinMappingFWHM"     \
+        --myelin-surface-fwhm="$SurfaceSmoothingFWHM"
 
     echo ". ${EnvironmentScript}"
 done
