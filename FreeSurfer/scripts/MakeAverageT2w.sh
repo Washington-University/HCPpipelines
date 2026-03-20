e1c7bab06edad004a33b22f8709e6c30820a6bba
MIkhail Milchenko <mmilch01@gmail.com>
2026-03-20 15:10:12 -0500

    Address: https://github.com/Washington-University/HCPpipelines/pull/389#discussion_r2948770224 https://github.com/Washington-University/HCPpipelines/pull/389#discussion_r2948803310 https://github.com/Washington-University/HCPpipelines/pull/389#discussion_r2948847403 https://github.com/Washington-University/HCPpipelines/pull/389#discussion_r2948854327 https://github.com/Washington-University/HCPpipelines/pull/389#discussion_r2948862965

--- FreeSurfer/scripts/MakeAverageT2w.sh
+++ FreeSurfer/scripts/MakeAverageT2w.sh
@@ -13,8 +13,11 @@ ${script_name}
 
 Usage: ${script_name} StudyFolder Subject SessionList TemplateID
 
-Create base template from T1w images of cross-sectional sessions. Coregister each T2w image to the base template.
-Make average T2w image and put it in the longitudinal template T2w/average folder.
+Create midpoint 'bootstrap' template image from T2w images of cross-sectional sessions. 
+Put it in the longitudinal template T2w folder.
+Note that the creation of T2w template is similar to that used in the base stage of recon-all, 
+but is not otherwise used.
+The resulting 'bootstrap' T2w template is used in the base stage of recon-all. 
 
 EOF
 }
@@ -56,7 +59,7 @@ log_Check_Env_Var CARET7DIR
 #  Start work
 # ------------------------------------------------------------------------------
 
-echo -e "\n START: MakeAverageT2w.sh"
+log_Msg "Starting MakeAverageT2w.sh"
 
 StudyFolder="$1"
 Subject="$2"
@@ -70,22 +73,20 @@ cmd=(mri_robust_template)
 cmd_mov=()
 cmd_lta=()
 
-
 Sessions=$(echo "${Sessions}" | sed 's/@/ /g')
 
 for session in ${Sessions[@]}; do
     cmd_mov+=("$StudyFolder/$session/T1w/$session/mri/orig/T2raw.mgz")
-    cmd_lta+=("$TemplateDir/T1w/xfms/${session}_t2w2base.lta")
+    cmd_lta+=("$TemplateDir/T2w/xfms/${session}_t2w2bootstrap_average.lta")
 done
 
 echo "${cmd_mov[@]}"
 
 cmd+=(--mov "${cmd_mov[@]}")
 cmd+=(--template "$TemplateDir/T2w/bootstrap_average.nii.gz" --iscale --satit)
-#uncomment this to output T2w->average template transforms
-#cmd+=(--lta ${cmd_lta[@]} --iscale --satit)
+cmd+=(--lta "${cmd_lta[@]}")
 
-mkdir -p "$TemplateDir/T2w"
+mkdir -p "$TemplateDir/T2w/xfms"
 
 echo "${cmd[@]}"
 "${cmd[@]}"
