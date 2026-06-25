678050e1e52302f7eb0dc330417a01dbc9569ccd
Alex Zhou <94024008+Azure2003@users.noreply.github.com>
2026-06-24 22:31:16 -0500

    Update SLURM script with placeholder values

--- MMORF/scripts/template.sh
+++ MMORF/scripts/template.sh
@@ -2,15 +2,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=5:00:00
-#SBATCH --account=matt_glasser
-#SBATCH --partition=tier2_gpu
+#SBATCH --account=XXX
+#SBATCH --partition=XXX
 #SBATCH --gres=gpu:1
 #SBATCH --mem=300G
 #SBATCH --cpus-per-gpu=8
-#SBATCH -x gpua401,gpua804,gpu04 
 
 module load cuda
 module load fsl/6.0.5
 
-source activate /home/alexander.z/miniconda3/envs/mmorf
+source activate XXX/miniconda3/envs/mmorf
 export FSLOUTPUTTYPE=NIFTI_GZ
