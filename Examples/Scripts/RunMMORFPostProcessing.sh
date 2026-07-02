4b42cd6f5122f25e0d596a5c5173ea610b8123d3
Azure2003 <alexander.haoyang.zhou@gmail.com>
2026-07-02 16:00:04 -0500

    New post porcessing so I don't have to do mass edits

---
+++ Examples/Scripts/RunMMORFPostProcessing.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+###Mandatory Arguments###
+StudyFolder="${HOME}/projects/HCPpipelines_ExampleData"
+Sessionlist="100307 100610"
+T1wTemplate="${TemplateDir}/MMORF_T1.nii.gz"
+
+#####################################
+EnvironmentScript="${HOME}/projects/HCPpipelines/Examples/Scripts/SetUpHCPPipeline.sh" #Pipeline environment script
+
+source "${EnvironmentScript}"
+
+
+QUEUE=""
+
+
+for Session in ${Sessionlist}; do
+    echo "Launching MMORF registration for session ${Session}"
+    $FSLDIR/bin/fsl_sub \
+    -q ${QUEUE} \
+    ${HCPPIPEDIR}/MMORF/MMORFPostProcessing.sh \
+    --study-folder="${StudyFolder}" \
+    --session="${Session}" \
+    --t1-template="${T1wTemplate}"
+done
