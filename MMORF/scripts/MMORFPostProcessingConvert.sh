be52a984a2c82a705c722b6ec16ba23fe36b81c3
Azure2003 <alexander.haoyang.zhou@gmail.com>
2026-07-13 20:27:10 -0500

    Redid mandatory things

--- MMORF/scripts/MMORFPostProcessingConvert.sh
+++ MMORF/scripts/MMORFPostProcessingConvert.sh
@@ -13,9 +13,9 @@ fi
 source "$HCPPIPEDIR/global/scripts/newopts.shlib" "$@"
 source "$HCPPIPEDIR/global/scripts/debug.shlib" "$@"
 opts_SetScriptDescription "Run MMORF registration for multiple sessions in parallel"
-opts_AddMandatory '--study-folder' 'StudyFolder' 'Path to the study folder containing session folders' ""
-opts_AddMandatory '--session' 'Session' 'Subject ID' ""
-opts_AddMandatory '--t1-template' 'T1wTemplate' 'Path to the T1w template image' ""
+opts_AddMandatory '--study-folder' 'StudyFolder' 'folder' 'Path to the study folder containing session folders'
+opts_AddMandatory '--session' 'Session' 'subject ID' '(e.g. 100610)'
+opts_AddMandatory '--t1-template' 'T1wTemplate' 'Image' "Path to the T1w template image"
 opts_ParseArguments "$@"
 
 if ((pipedirguessed))
