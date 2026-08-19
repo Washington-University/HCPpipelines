9e66de157afb026568ea6de8563de7de8c291e30
Burke Rosen <bqrosen@wustl.edu>
2026-08-19 15:18:04 -0500

    refactor to fix indentation and rearrange code blocks for readability

--- PFM/scripts/PostPROFUMO.m
+++ PFM/scripts/PostPROFUMO.m
@@ -36,95 +36,96 @@ for s = 1:numel(Subjlist)
     % If ConcatName is specified, use concatenated version; otherwise check individual runs
     subfMRINames = {};
     if ~strcmp(ConcatName, '')
-    % Multi-run data: check if concatenated dataset exists
-    if exist([StudyFolder '/' Subjlist{s} '/MNINonLinear/Results/' ConcatName '/' ConcatName fMRIProcSTRING '.dtseries.nii'],'file')
+        % Multi-run data: check if concatenated dataset exists
+        if exist([StudyFolder '/' Subjlist{s} '/MNINonLinear/Results/' ConcatName '/' ConcatName fMRIProcSTRING '.dtseries.nii'],'file')
+            c = 1;
+            for r = 1:numel(fMRINames)
+                if exist([StudyFolder '/' Subjlist{s} '/MNINonLinear/Results/' fMRINames{r} '/' fMRINames{r} fMRIProcSTRING '.dtseries.nii'],'file')
+                    subfMRINames{c} = fMRINames{r};
+                    c = c + 1;
+                end
+            end  % for r = 1:numel(fMRINames)            
+        end
+    else
+        % Single-run data: check which runs exist
         c = 1;
         for r = 1:numel(fMRINames)
-        if exist([StudyFolder '/' Subjlist{s} '/MNINonLinear/Results/' fMRINames{r} '/' fMRINames{r} fMRIProcSTRING '.dtseries.nii'],'file')
-            subfMRINames{c} = fMRINames{r};
-            c = c + 1;
-        end
-        end  % for r = 1:numel(fMRINames)            
-    end
-    else
-    % Single-run data: check which runs exist
-    c = 1;
-    for r = 1:numel(fMRINames)
-        if exist([StudyFolder '/' Subjlist{s} '/MNINonLinear/Results/' fMRINames{r} '/' fMRINames{r} fMRIProcSTRING '.dtseries.nii'],'file')
-        subfMRINames{c} = fMRINames{r};
-        c = c + 1;
-        end
-    end  % for r = 1:numel(fMRINames)
+            if exist([StudyFolder '/' Subjlist{s} '/MNINonLinear/Results/' fMRINames{r} '/' fMRINames{r} fMRIProcSTRING '.dtseries.nii'],'file')
+                subfMRINames{c} = fMRINames{r};
+                c = c + 1;
+            end
+        end  % for r = 1:numel(fMRINames)
     end
   
     %% Process subject if valid runs found
     if numel(subfMRINames) ~= 0
-    %% Load and concatenate PFM time courses and amplitudes
-    % Load PROFUMO outputs and amplitude-modulate time courses
-    origTCS = [];  % Original unmodulated time courses
-    TCS = [];      % Amplitude-modulated time courses
-    for r = 1:numel(subfMRINames)
-        runTCS = load([PFMFolder '/Results.ppp/TimeCourses/sub-' Subjlist{s} '_run-' subfMRINames{r} '.csv']);
-        runAmp = load([PFMFolder '/Results.ppp/Amplitudes/sub-' Subjlist{s} '_run-' subfMRINames{r} '.csv']);
+        %% Load and concatenate PFM time courses and amplitudes
+        % Load PROFUMO outputs and amplitude-modulate time courses
+        origTCS = [];  % Original unmodulated time courses
+        TCS = [];      % Amplitude-modulated time courses
+        for r = 1:numel(subfMRINames)
+            runTCS = load([PFMFolder '/Results.ppp/TimeCourses/sub-' Subjlist{s} '_run-' subfMRINames{r} '.csv']);
+            runAmp = load([PFMFolder '/Results.ppp/Amplitudes/sub-' Subjlist{s} '_run-' subfMRINames{r} '.csv']);
 
-        origTCS = [origTCS ; runTCS];
-        TCS = [TCS ; runTCS .* repmat(runAmp', size(runTCS, 1), 1)];
-    end  % for r = 1:numel(subfMRINames)
-    
-    %% Create original time course and spectral CIFTI files
-    % Generate CIFTI structure for unmodulated time courses
-    PFMTCSorig = cifti_struct_create_sdseries(origTCS','step',TR);
-    
-    % Store power spectra 
-    ts.Nnodes = size(origTCS, 2);
-    ts.Nsubjects = 1;
-    ts.ts = origTCS;
-    ts.NtimepointsPerSubject = size(origTCS, 1);
-    PFMSpectraorig = cifti_struct_create_sdseries(nets_spectra_sp(ts)','step',1/TR);
-    
-    %% Create  time course and spectral CIFTI files
-    % Generate CIFTI structure for  time courses
-    PFMTCS = cifti_struct_create_sdseries(TCS','step',TR);
-    
-    % Store power spectra 
-    ts.Nnodes = size(TCS, 2);
-    ts.Nsubjects = 1;
-    ts.ts = TCS;
-    ts.NtimepointsPerSubject = size(TCS, 1);
-    PFMSpectra = cifti_struct_create_sdseries(nets_spectra_sp(ts)','step',1/TR);
+            origTCS = [origTCS ; runTCS];
+            TCS = [TCS ; runTCS .* repmat(runAmp', size(runTCS, 1), 1)];
+            end  % for r = 1:numel(subfMRINames)
+        
+        %% Create original time course and spectral CIFTI files
+        % Generate CIFTI structure for unmodulated time courses
+        PFMTCSorig = cifti_struct_create_sdseries(origTCS','step',TR);
+        
+        % Store power spectra 
+        ts.Nnodes = size(origTCS, 2);
+        ts.Nsubjects = 1;
+        ts.ts = origTCS;
+        ts.NtimepointsPerSubject = size(origTCS, 1);
+        PFMSpectraorig = cifti_struct_create_sdseries(nets_spectra_sp(ts)','step',1/TR);
+        
+        %% Create  time course and spectral CIFTI files
+        % Generate CIFTI structure for  time courses
+        PFMTCS = cifti_struct_create_sdseries(TCS','step',TR);
+        
+        % Store power spectra 
+        ts.Nnodes = size(TCS, 2);
+        ts.Nsubjects = 1;
+        ts.ts = TCS;
+        ts.NtimepointsPerSubject = size(TCS, 1);
+        PFMSpectra = cifti_struct_create_sdseries(nets_spectra_sp(ts)','step',1/TR);
 
-    %% restore variance
-    if VarNormBool
-        fprintf('Restoring variance\n');
-        clean_VN_Name = [StudyFolder '/' Subjlist{s} '/MNINonLinear/Results/' ConcatName '/' ConcatName RegString fMRIProcSTRING '_vn.dscalar.nii'];
-        clean_VN = ciftiopen(clean_VN_Name, wbcommand).cdata;
-        mapFile = [PFMFolder '/Results.ppp/Maps/sub-' Subjlist{s} '.dscalar.nii'];
-        maps = ciftiopen(mapFile, wbcommand);
-        maps.cdata = maps.cdata .* clean_VN;
-        ciftisave(maps, mapFile, wbcommand);
-    end
+        %% Save non-map results
+        % Save original and amplitude-modulated time courses and spectra
+        ciftisave(PFMTCSorig, [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_ts_orig.' LowResMesh 'k_fs_LR.sdseries.nii'], wbcommand);
+        ciftisave(PFMSpectraorig, [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_spectra_orig.' LowResMesh 'k_fs_LR.sdseries.nii'], wbcommand);
 
-    %% divide out vertex area weights
-    if VAweightBool
-        fprintf('Dividing out vertex area weights\n');
-        VAgray = ciftiopen([StudyFolder '/' Subjlist{s} '/T1w/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.midthickness' RegString '_va_norm.grayordinates.' LowResMesh 'k_fs_LR.dscalar.nii'], wbcommand).cdata;
-        mapFile = [PFMFolder '/Results.ppp/Maps/sub-' Subjlist{s} '.dscalar.nii'];
-        maps = ciftiopen(mapFile, wbcommand);
-        maps.cdata = maps.cdata ./ VAgray;
-        ciftisave(maps, mapFile, wbcommand);
-    end
+        ciftisave(PFMTCS, [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_ts.' LowResMesh 'k_fs_LR.sdseries.nii'], wbcommand);
+        ciftisave(PFMSpectra, [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_spectra.' LowResMesh 'k_fs_LR.sdseries.nii'], wbcommand);
 
-    %% Save individual-level results
-    % Save original and amplitude-modulated time courses and spectra
-    ciftisave(PFMTCSorig, [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_ts_orig.' LowResMesh 'k_fs_LR.sdseries.nii'], wbcommand);
-    ciftisave(PFMSpectraorig, [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_spectra_orig.' LowResMesh 'k_fs_LR.sdseries.nii'], wbcommand);
+        %% Handle maps
+        % restore variance
+        if VarNormBool
+            fprintf('Restoring variance\n');
+            clean_VN_Name = [StudyFolder '/' Subjlist{s} '/MNINonLinear/Results/' ConcatName '/' ConcatName RegString fMRIProcSTRING '_vn.dscalar.nii'];
+            clean_VN = ciftiopen(clean_VN_Name, wbcommand).cdata;
+            mapFile = [PFMFolder '/Results.ppp/Maps/sub-' Subjlist{s} '.dscalar.nii'];
+            maps = ciftiopen(mapFile, wbcommand);
+            maps.cdata = maps.cdata .* clean_VN;
+            ciftisave(maps, mapFile, wbcommand);
+        end
 
-    ciftisave(PFMTCS, [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_ts.' LowResMesh 'k_fs_LR.sdseries.nii'], wbcommand);
-    ciftisave(PFMSpectra, [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_spectra.' LowResMesh 'k_fs_LR.sdseries.nii'], wbcommand);
+        % divide out vertex area weights
+        if VAweightBool
+            fprintf('Dividing out vertex area weights\n');
+            VAgray = ciftiopen([StudyFolder '/' Subjlist{s} '/T1w/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.midthickness' RegString '_va_norm.grayordinates.' LowResMesh 'k_fs_LR.dscalar.nii'], wbcommand).cdata;
+            mapFile = [PFMFolder '/Results.ppp/Maps/sub-' Subjlist{s} '.dscalar.nii'];
+            maps = ciftiopen(mapFile, wbcommand);
+            maps.cdata = maps.cdata ./ VAgray;
+            ciftisave(maps, mapFile, wbcommand);
+        end
+        
+        % Copy PFM maps from PFM folder to subject's MNINonLinear/fsaverage_LR<LowResMesh> space directory
+        copyfile([PFMFolder '/Results.ppp/Maps/sub-' Subjlist{s} '.dscalar.nii'], [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_origmaps.' LowResMesh 'k_fs_LR.dscalar.nii']);
 
-    %% Copy individual PFM maps
-    % Link PROFUMO spatial maps to subject's fsaverage space directory
-    copyfile([PFMFolder '/Results.ppp/Maps/sub-' Subjlist{s} '.dscalar.nii'], [StudyFolder '/' Subjlist{s} '/MNINonLinear/fsaverage_LR' LowResMesh 'k/' Subjlist{s} '.' OutputSTRING RegString '_origmaps.' LowResMesh 'k_fs_LR.dscalar.nii']);
     end  % if numel(subfMRINames) ~= 0
 end  % for s = 1:numel(Subjlist)
 end
