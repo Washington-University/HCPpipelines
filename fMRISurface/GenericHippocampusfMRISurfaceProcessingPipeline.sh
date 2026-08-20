cbc99436d14bad263c6a6f94233262f02a613b46
brainmappers <brainmappers@brainmappers-desktop2.wustl.edu>
2026-08-20 13:07:00 -0500

    Add hippocampal fMRISurface processing

---
+++ fMRISurface/GenericHippocampusfMRISurfaceProcessingPipeline.sh
@@ -0,0 +1,216 @@
+#!/bin/bash
+
+# Requirements for this script
+# installed versions of: FSL, Connectome Workbench (wb_command)
+# environment: HCPPIPEDIR, FSLDIR, CARET7DIR
+
+########################################## PIPELINE OVERVIEW ##########################################
+
+# TODO
+
+########################################## OUTPUT DIRECTORIES ##########################################
+
+# TODO
+
+################################################ SUPPORT FUNCTIONS ##################################################
+
+set -eu
+pipedirguessed=0
+if [[ "${HCPPIPEDIR:-}" == "" ]]
+then
+    pipedirguessed=1
+    # Fix this if the script is more than one level below HCPPIPEDIR
+    export HCPPIPEDIR="$(dirname -- "$0")/.."
+fi
+
+#comment the line back in when done
+#source "${HCPPIPEDIR}/global/scripts/debug.shlib" "$@"          # Debugging functions; also sources log.shlib
+source "${HCPPIPEDIR}/global/scripts/log.shlib" "$@"         # Debugging functions; also sources log.shlib
+source "${HCPPIPEDIR}/global/scripts/newopts.shlib" "$@"
+source "${HCPPIPEDIR}/global/scripts/processingmodecheck.shlib" # Check processing mode requirements
+
+opts_SetScriptDescription "Run hippocampal fMRISurface processing"
+
+opts_AddMandatory '--studyfolder' 'Path' 'path' "folder containing all subjects" "--path"
+
+opts_AddMandatory '--subject' 'Subject' 'subject ID' ""
+
+opts_AddMandatory '--fmriname' 'NameOffMRI' 'string' 'fMRI run name, for example rfMRI_REST1_LR'
+
+opts_AddOptional '--procstring' 'ProcString' 'string' 'processing suffix appended to the 4D fMRI filename; include the leading underscore' ""
+
+opts_AddMandatory '--smoothingFWHM' 'SmoothingFWHM' 'number' 'smoothing FWHM (mm)'
+
+opts_AddOptional '--fmri-qc' 'QCMode' 'YES OR NO OR ONLY' "Controls whether to generate a QC scene and snapshots (default=YES). ONLY executes just the QC script." "YES"
+
+opts_AddOptional '--output-directory' 'OutputDirectory' 'path' 'Directory where pipeline outputs will be written' ""
+
+opts_AddOptional '--goodvoxel' 'doGoodVoxels' 'YES OR NO' "Controls whether to do goodVoxel procedure (default = YES)" "YES"
+
+opts_AddOptional '--factor' 'factor' 'number' "Scaling factor for eliminating high COV voxels"
+
+opts_ParseArguments "$@"
+
+if ((pipedirguessed))
+then
+    log_Err_Abort "HCPPIPEDIR is not set; first source your edited copy of Examples/Scripts/SetUpHCPPipeline.sh"
+fi
+
+opts_ShowValues
+
+"${HCPPIPEDIR}/show_version"
+
+# ------------------------------------------------------------------------------
+# Verify required environment variables are set
+# ------------------------------------------------------------------------------
+
+log_Check_Env_Var HCPPIPEDIR
+log_Check_Env_Var FSLDIR
+log_Check_Env_Var CARET7DIR
+
+HCPPIPEDIR_fMRISurf="${HCPPIPEDIR}/fMRISurface/scripts"
+
+# ------------------------------------------------------------------------------
+# Parse command-line options
+# ------------------------------------------------------------------------------
+
+log_Msg "Platform Information Follows:"
+uname -a
+
+QCMode="$(echo "${QCMode}" | tr '[:upper:]' '[:lower:]')"
+
+doProcessing=1
+doQC=1
+
+case "${QCMode}" in
+    yes)
+        ;;
+    no)
+        doQC=0
+        ;;
+    only)
+        doProcessing=0
+        log_Warn "Only generating fMRI QC scene and snapshots from existing data"
+        ;;
+    *)
+        log_Err_Abort "Unrecognized value '${QCMode}' for --fmri-qc; use YES, NO, or ONLY"
+        ;;
+esac
+
+
+# ------------------------------------------------------------------------------
+# Set up paths
+# ------------------------------------------------------------------------------
+
+PipelineScripts="${HCPPIPEDIR_fMRISurf}"
+
+MainSubjectFolder="${Path}/${Subject}"
+AtlasSpaceFolder="${MainSubjectFolder}/MNINonLinear"
+InputResultsFolder="${AtlasSpaceFolder}/Results/${NameOffMRI}"
+ResultsFolderName="Results"
+
+if [ -n "${OutputDirectory}" ]; then
+    ResultsFolder="${OutputDirectory}/${Subject}/${NameOffMRI}"
+else
+    ResultsFolder="${AtlasSpaceFolder}/${ResultsFolderName}/${NameOffMRI}"
+fi
+
+WorkingDirectory="${ResultsFolder}/tmp"
+
+mkdir -p "${WorkingDirectory}"
+
+VolumefMRI="${InputResultsFolder}/${NameOffMRI}${ProcString}"
+
+SBRef="${InputResultsFolder}/${NameOffMRI}_SBRef.nii.gz"
+
+HippUnfoldFolder="${AtlasSpaceFolder}/HippUnfold"
+
+if [ ! -f "${VolumefMRI}.nii.gz" ]; then
+    log_Err_Abort "Cannot find selected fMRI file: ${VolumefMRI}.nii.gz"
+fi
+
+if [ ! -f "${SBRef}" ]; then
+    log_Err_Abort "Cannot find selected SBRef file: ${SBRef}"
+fi
+
+log_Msg "Selected fMRI file: ${VolumefMRI}.nii.gz"
+log_Msg "Selected SBRef file: ${SBRef}"
+
+# ------------------------------------------------------------------------------
+# Generate fMRI time series within ROIs
+# ------------------------------------------------------------------------------
+
+if ((doProcessing)); then
+
+    # Make fMRI ribbon
+    log_Msg "Make fMRI Ribbon"
+    log_Msg "mkdir -p ${WorkingDirectory}"
+
+    log_Msg "Hippocampal Volume To Surface Mapping"
+    "${PipelineScripts}/HippocampalVolumeToSurfaceMapping.sh" \
+            "${WorkingDirectory}" \
+            "${Subject}" \
+            "${HippUnfoldFolder}" \
+            "${VolumefMRI}" \
+            "${SBRef}" \
+            "${doGoodVoxels}" \
+            "${factor}"
+
+#Surface Smoothing
+ # ------------------------------------------------------------------------------
+# Hippocampal Surface Smoothing
+# ------------------------------------------------------------------------------
+
+    log_Msg "Hippocampal Surface Smoothing"
+
+    Sigma=`echo "$SmoothingFWHM / (2 * sqrt(2 * l(2)))" | bc -l`
+
+    Meshes=(native 512 2k 8k 18k)
+    Structures=(hipp dentate)
+
+    for Mesh in "${Meshes[@]}"; do
+        for Structure in "${Structures[@]}"; do
+            for Hemisphere in L R; do
+                "${CARET7DIR}/wb_command" -metric-smoothing \
+                    "${HippUnfoldFolder}/${Mesh}/${Subject}.${Hemisphere}.${Structure}_midthickness.${Mesh}.surf.gii" \
+                    "${WorkingDirectory}/${Subject}.${Hemisphere}.${Structure}_fMRI.${Mesh}.func.gii" \
+                    "${Sigma}" \
+                    "${WorkingDirectory}/${Subject}.${Hemisphere}.${Structure}_fMRI_s${SmoothingFWHM}.${Mesh}.func.gii" \
+                    -roi "${WorkingDirectory}/${Subject}.${Hemisphere}.${Structure}_ones.${Mesh}.func.gii"
+            done
+        done
+    done
+# ------------------------------------------------------------------------------
+# Combining ROIs into a single CIFTI file
+# ------------------------------------------------------------------------------
+
+
+    for Mesh in ${Meshes[@]}; do
+        "${CARET7DIR}/wb_command" -cifti-create-dense-timeseries \
+            "${ResultsFolder}/${NameOffMRI}_AtlasHipp_${ProcString}.${Mesh}.dtseries.nii" \
+            -metric HIPPOCAMPUS_LEFT "${WorkingDirectory}/${Subject}.L.hipp_fMRI_s${SmoothingFWHM}.${Mesh}.func.gii" \
+                -roi "${WorkingDirectory}/${Subject}.L.hipp_ones.${Mesh}.func.gii" \
+            -metric HIPPOCAMPUS_RIGHT "${WorkingDirectory}/${Subject}.R.hipp_fMRI_s${SmoothingFWHM}.${Mesh}.func.gii" \
+                -roi "${WorkingDirectory}/${Subject}.R.hipp_ones.${Mesh}.func.gii" \
+            -metric HIPPOCAMPUS_DENTATE_LEFT "${WorkingDirectory}/${Subject}.L.dentate_fMRI_s${SmoothingFWHM}.${Mesh}.func.gii" \
+                -roi "${WorkingDirectory}/${Subject}.L.dentate_ones.${Mesh}.func.gii" \
+            -metric HIPPOCAMPUS_DENTATE_RIGHT "${WorkingDirectory}/${Subject}.R.dentate_fMRI_s${SmoothingFWHM}.${Mesh}.func.gii" \
+                -roi "${WorkingDirectory}/${Subject}.R.dentate_ones.${Mesh}.func.gii"
+
+            log_Msg "Generated fMRI time series Cifti file: "${ResultsFolder}/${Subject}.${NameOffMRI}"_AtlasHipp_${ProcString}.${Mesh}.dtseries.nii"
+    done
+fi
+
+#Uncomment if want to remove the subfoler with intermediate files
+#rm -rf "${WorkingDirectory}"
+
+# if ((doQC)); then
+#     log_Msg "Generating fMRI QC scene and snapshots"
+#     "${PipelineScripts}/GenerateFMRIScenes.sh" \
+#         --study-folder="${Path}" \
+#         --subject="${Subject}" \
+#         --fmriname="${NameOffMRI}${ProcString}" \
+#         --output-folder="${ResultsFolder}/fMRIQC"
+# fi
+
+log_Msg "Completed!"
---
+++ fMRISurface/scripts/HippocampalVolumeToSurfaceMapping.sh
@@ -0,0 +1,538 @@
+#!/bin/bash 
+
+# --------------------------------------------------------------------------------
+#  Usage Description Function
+# --------------------------------------------------------------------------------
+
+script_name=$(basename "${0}")
+
+show_usage() {
+    cat <<EOF
+
+${script_name}: Sub-script of GenericHippocampusfMRISurfaceProcessingPipeline.sh
+
+EOF
+}
+
+
+# Allow script to return a Usage statement, before any other output or checking
+if [ "$#" = "0" ]; then
+    show_usage
+    exit 1
+fi
+
+# ------------------------------------------------------------------------------
+#  Check that HCPPIPEDIR is defined and Load Function Libraries
+# ------------------------------------------------------------------------------
+
+if [ -z "${HCPPIPEDIR}" ]; then
+    echo "${script_name}: ABORTING: HCPPIPEDIR environment variable must be set"
+    exit 1
+fi
+
+#source "${HCPPIPEDIR}/global/scripts/debug.shlib" "$@"         # Debugging functions; also sources log.shlib
+source "${HCPPIPEDIR}/global/scripts/log.shlib" "$@"            # Debugging functions; also sources log.shlib
+source "${HCPPIPEDIR}/global/scripts/opts.shlib"                # Command line option functions
+
+opts_ShowVersionIfRequested "$@"
+
+if opts_CheckForHelpRequest "$@"; then
+    show_usage
+    exit 0
+fi
+
+# ------------------------------------------------------------------------------
+#  Verify required environment variables are set and log value
+# ------------------------------------------------------------------------------
+
+log_Check_Env_Var HCPPIPEDIR
+log_Check_Env_Var CARET7DIR
+
+# ------------------------------------------------------------------------------
+#  Start work
+# ------------------------------------------------------------------------------
+
+WorkingDirectory="$1"
+Subject="$2"
+HippUnfoldFolder="$3"
+VolumefMRI="$4"
+SBRef="$5"
+doGoodVoxels="$6"
+Factor="$7"
+#Factor="1"                 # Factor is a scaling factor for how many std units away from mean to set threshold
+NeighborhoodSmoothing="5"   # Distinguishes large vs small dropout
+dilation_dist="10"
+
+Meshes=(512 2k 8k 18k)
+
+WB="${CARET7DIR}/wb_command"
+
+mkdir -p "${WorkingDirectory}"
+
+FirstRibbon="YES"
+
+# ------------------------------------------------------------------------------
+#  Generating mean, std, and coefficient of variation metric maps
+# ------------------------------------------------------------------------------
+
+fslmaths "${VolumefMRI}" \
+    -Tmean "${WorkingDirectory}/${Subject}.mean" \
+    -odt float
+
+fslmaths "${VolumefMRI}" \
+    -Tstd "${WorkingDirectory}/${Subject}.std" \
+    -odt float
+
+fslmaths "${WorkingDirectory}/${Subject}.std" \
+    -div "${WorkingDirectory}/${Subject}.mean" \
+    "${WorkingDirectory}/${Subject}.cov"
+
+
+for Structure in hipp dentate; do
+
+    for Hemisphere in L R; do
+
+        # ------------------------------------------------------------------------------
+        #  Establishing input and output files
+        # ------------------------------------------------------------------------------
+
+        Prefix="${Subject}.${Hemisphere}.${Structure}"
+
+        ThicknessMetric="${HippUnfoldFolder}/native/${Prefix}_thickness.native.shape.gii"
+
+        InnerSurface="${HippUnfoldFolder}/native/${Prefix}_inner.native.surf.gii"
+        MidSurface="${HippUnfoldFolder}/native/${Prefix}_midthickness.native.surf.gii"
+        OuterSurface="${HippUnfoldFolder}/native/${Prefix}_outer.native.surf.gii"
+
+        OnesMetric="${WorkingDirectory}/${Prefix}_ones.native.func.gii"
+        ProbabilisticRibbon="${WorkingDirectory}/${Prefix}.ribbon_probabilistic.nii.gz"
+        BinaryRibbon="${WorkingDirectory}/${Prefix}.ribbon.nii.gz"
+
+
+        # ------------------------------------------------------------------------------
+        #  Generating an all-ones surface metric
+        # ------------------------------------------------------------------------------
+
+        "${WB}" -metric-math \
+            '1' \
+            "${OnesMetric}" \
+            -var x \
+            "${ThicknessMetric}"
+
+
+        # ------------------------------------------------------------------------------
+        #  Mapping the all-ones metric into SBRef volume space using the inner and
+        #  outer surfaces as ribbon boundaries
+        # ------------------------------------------------------------------------------
+
+        "${WB}" -metric-to-volume-mapping \
+            "${OnesMetric}" \
+            "${MidSurface}" \
+            "${SBRef}" \
+            "${ProbabilisticRibbon}" \
+            -ribbon-constrained \
+                "${InnerSurface}" \
+                "${OuterSurface}"
+
+
+        # ------------------------------------------------------------------------------
+        #  Thresholding the fractional ribbon at greater than 0 to create a binary mask
+        # ------------------------------------------------------------------------------
+
+        "${WB}" -volume-math \
+            "x > 0" \
+            "${BinaryRibbon}" \
+            -var x \
+            "${ProbabilisticRibbon}"
+
+
+        # ------------------------------------------------------------------------------
+        #  Generating good-voxel mask for each structure
+        # ------------------------------------------------------------------------------
+
+        if [[ "${doGoodVoxels}" == "YES" ]]; then
+
+            fslmaths "${WorkingDirectory}/${Subject}.cov" \
+                -mas "${BinaryRibbon}" \
+                "${WorkingDirectory}/${Prefix}_cov_ribbon"
+
+            meanIntensity=$(fslstats "${WorkingDirectory}/${Prefix}_cov_ribbon" -M)
+
+            fslmaths "${WorkingDirectory}/${Prefix}_cov_ribbon" \
+                -div ${meanIntensity} \
+                "${WorkingDirectory}/${Prefix}_cov_ribbon_norm"
+
+            fslmaths "${WorkingDirectory}/${Prefix}_cov_ribbon_norm" \
+                -bin \
+                -s "${NeighborhoodSmoothing}" \
+                "${WorkingDirectory}/${Prefix}_SmoothNorm"
+
+            fslmaths "${WorkingDirectory}/${Prefix}_cov_ribbon_norm" \
+                -s "${NeighborhoodSmoothing}" \
+                -div "${WorkingDirectory}/${Prefix}_SmoothNorm" \
+                -dilD \
+                "${WorkingDirectory}/${Prefix}_cov_ribbon_norm_s${NeighborhoodSmoothing}"
+
+            fslmaths "${WorkingDirectory}/${Subject}.cov" \
+                -div ${meanIntensity} \
+                -div "${WorkingDirectory}/${Prefix}_cov_ribbon_norm_s${NeighborhoodSmoothing}" \
+                "${WorkingDirectory}/${Prefix}_cov_norm_modulate"
+
+            fslmaths "${WorkingDirectory}/${Prefix}_cov_norm_modulate" \
+                -mas "${BinaryRibbon}" \
+                "${WorkingDirectory}/${Prefix}_cov_norm_modulate_ribbon"
+
+            STD=$(fslstats \
+                "${WorkingDirectory}/${Prefix}_cov_norm_modulate_ribbon" \
+                -S)
+
+            echo "STD: ${STD}"
+
+            MEAN=$(fslstats \
+                "${WorkingDirectory}/${Prefix}_cov_norm_modulate_ribbon" \
+                -M)
+
+            echo "MEAN: ${MEAN}"
+
+            Lower=$(echo "${MEAN} - (${STD} * ${Factor})" | bc -l)
+            echo "LOWER: ${Lower}"
+
+            Upper=$(echo "${MEAN} + (${STD} * ${Factor})" | bc -l)
+            echo "UPPER: ${Upper}"
+
+            fslmaths "${WorkingDirectory}/${Subject}.mean" \
+                -bin \
+                "${WorkingDirectory}/${Prefix}_mask"
+
+            fslmaths "${WorkingDirectory}/${Prefix}_cov_norm_modulate" \
+                -thr "${Upper}" \
+                -bin \
+                -sub "${WorkingDirectory}/${Prefix}_mask" \
+                -mul -1 \
+                -mas "${BinaryRibbon}" \
+                "${WorkingDirectory}/${Prefix}_goodvoxels"
+        fi
+
+
+        NativeFolder="${HippUnfoldFolder}/native"
+
+        InnerSurface="${NativeFolder}/${Prefix}_inner.native.surf.gii"
+        MidSurface="${NativeFolder}/${Prefix}_midthickness.native.surf.gii"
+        OuterSurface="${NativeFolder}/${Prefix}_outer.native.surf.gii"
+
+        NativeFlat="${NativeFolder}/${Prefix}_flat.native.surf.gii"
+        NativeSurfArea="${NativeFolder}/${Prefix}_surfarea.native.shape.gii"
+
+        NativeROI="${WorkingDirectory}/${Prefix}_ones.native.func.gii"
+
+
+        # =====================================================================
+        # Map mean and covariance volumes
+        # =====================================================================
+
+        for Map in mean cov; do
+
+            NativeMetric="${WorkingDirectory}/${Prefix}_${Map}.native.func.gii"
+            NativeAllMetric="${WorkingDirectory}/${Prefix}_${Map}_all.native.func.gii"
+
+
+            # -----------------------------------------------------------------
+            # Map using good voxels
+            # -----------------------------------------------------------------
+
+            if [[ "${doGoodVoxels}" == "YES" ]]; then
+
+                "${WB}" -volume-to-surface-mapping \
+                    "${WorkingDirectory}/${Subject}.${Map}.nii.gz" \
+                    "${MidSurface}" \
+                    "${NativeMetric}" \
+                    -ribbon-constrained \
+                        "${InnerSurface}" \
+                        "${OuterSurface}" \
+                        -volume-roi "${WorkingDirectory}/${Prefix}_goodvoxels.nii.gz" \
+                        -dilate-missing ${dilation_dist} \
+                            -nearest
+
+            else
+
+                "${WB}" -volume-to-surface-mapping \
+                    "${WorkingDirectory}/${Subject}.${Map}.nii.gz" \
+                    "${MidSurface}" \
+                    "${NativeMetric}" \
+                    -ribbon-constrained \
+                        "${InnerSurface}" \
+                        "${OuterSurface}" \
+                        -dilate-missing ${dilation_dist} \
+                            -nearest
+            fi
+
+            "${WB}" -metric-dilate \
+                "${NativeMetric}" \
+                "${MidSurface}" \
+                "${dilation_dist}" \
+                "${NativeMetric}" \
+                -nearest
+
+            "${WB}" -metric-mask \
+                "${NativeMetric}" \
+                "${NativeROI}" \
+                "${NativeMetric}"
+
+
+            # -----------------------------------------------------------------
+            # Map using all ribbon voxels
+            # -----------------------------------------------------------------
+
+            "${WB}" -volume-to-surface-mapping \
+                "${WorkingDirectory}/${Subject}.${Map}.nii.gz" \
+                "${MidSurface}" \
+                "${NativeAllMetric}" \
+                -ribbon-constrained \
+                    "${InnerSurface}" \
+                    "${OuterSurface}" \
+                    -dilate-missing ${dilation_dist} \
+                        -nearest
+
+            "${WB}" -metric-mask \
+                "${NativeAllMetric}" \
+                "${NativeROI}" \
+                "${NativeAllMetric}"
+
+
+            # -----------------------------------------------------------------
+            # Resample native metrics using unfolded flat surfaces
+            # -----------------------------------------------------------------
+
+            for Mesh in ${Meshes[@]}; do
+
+                MeshFolder="${HippUnfoldFolder}/${Mesh}"
+
+                TargetFlat="${MeshFolder}/${Prefix}_flat.${Mesh}.surf.gii"
+                TargetMidSurface="${MeshFolder}/${Prefix}_midthickness.${Mesh}.surf.gii"
+                TargetSurfArea="${MeshFolder}/${Prefix}_surfarea.${Mesh}.shape.gii"
+
+                TargetROI="${WorkingDirectory}/${Prefix}_ones.${Mesh}.func.gii"
+
+                TargetMetric="${WorkingDirectory}/${Prefix}_${Map}.${Mesh}.func.gii"
+                TargetAllMetric="${WorkingDirectory}/${Prefix}_${Map}_all.${Mesh}.func.gii"
+
+
+                "${WB}" -metric-math \
+                    "x * 0 + 1" \
+                    "${TargetROI}" \
+                    -var x "${TargetSurfArea}"
+
+
+                "${WB}" -metric-resample \
+                    "${NativeMetric}" \
+                    "${NativeFlat}" \
+                    "${TargetFlat}" \
+                    ADAP_BARY_AREA \
+                    "${TargetMetric}" \
+                    -area-surfs \
+                        "${MidSurface}" \
+                        "${TargetMidSurface}" \
+                    -current-roi "${NativeROI}" \
+                    -bypass-sphere-check
+
+                "${WB}" -metric-mask \
+                    "${TargetMetric}" \
+                    "${TargetROI}" \
+                    "${TargetMetric}"
+
+
+                "${WB}" -metric-resample \
+                    "${NativeAllMetric}" \
+                    "${NativeFlat}" \
+                    "${TargetFlat}" \
+                    ADAP_BARY_AREA \
+                    "${TargetAllMetric}" \
+                    -area-surfs \
+                        "${MidSurface}" \
+                        "${TargetMidSurface}" \
+                    -current-roi "${NativeROI}" \
+                    -bypass-sphere-check
+
+                "${WB}" -metric-mask \
+                    "${TargetAllMetric}" \
+                    "${TargetROI}" \
+                    "${TargetAllMetric}"
+
+
+        done
+
+
+        # =====================================================================
+        # Map goodvoxels in volume space
+        # =====================================================================
+
+        if [[ "${doGoodVoxels}" == "YES" ]]; then
+
+            NativeGoodVoxels="${WorkingDirectory}/${Prefix}_goodvoxels.native.func.gii"
+
+            "${WB}" -volume-to-surface-mapping \
+                "${WorkingDirectory}/${Prefix}_goodvoxels.nii.gz" \
+                "${MidSurface}" \
+                "${NativeGoodVoxels}" \
+                -ribbon-constrained \
+                    "${InnerSurface}" \
+                    "${OuterSurface}" \
+                    -dilate-missing ${dilation_dist} \
+                        -nearest
+
+            "${WB}" -metric-mask \
+                "${NativeGoodVoxels}" \
+                "${NativeROI}" \
+                "${NativeGoodVoxels}"
+
+
+            for Mesh in ${Meshes[@]}; do
+
+                MeshFolder="${HippUnfoldFolder}/${Mesh}"
+
+                TargetFlat="${MeshFolder}/${Prefix}_flat.${Mesh}.surf.gii"
+                TargetMidSurface="${MeshFolder}/${Prefix}_midthickness.${Mesh}.surf.gii"
+                TargetROI="${WorkingDirectory}/${Prefix}_ones.${Mesh}.func.gii"
+
+                TargetGoodVoxels="${WorkingDirectory}/${Prefix}_goodvoxels.${Mesh}.func.gii"
+
+
+                "${WB}" -metric-resample \
+                    "${NativeGoodVoxels}" \
+                    "${NativeFlat}" \
+                    "${TargetFlat}" \
+                    ADAP_BARY_AREA \
+                    "${TargetGoodVoxels}" \
+                    -area-surfs \
+                        "${MidSurface}" \
+                        "${TargetMidSurface}" \
+                    -current-roi "${NativeROI}" \
+                    -bypass-sphere-check
+
+                "${WB}" -metric-mask \
+                    "${TargetGoodVoxels}" \
+                    "${TargetROI}" \
+                    "${TargetGoodVoxels}"
+
+            done
+
+        fi
+
+
+        # =====================================================================
+        # Map complete fMRI timeseries to native hippocampal surface
+        # =====================================================================
+
+        NativefMRI="${WorkingDirectory}/${Prefix}_fMRI.native.func.gii"
+
+
+        if [[ "${doGoodVoxels}" == "YES" ]]; then
+
+            "${WB}" -volume-to-surface-mapping \
+                "${VolumefMRI}.nii.gz" \
+                "${MidSurface}" \
+                "${NativefMRI}" \
+                -ribbon-constrained \
+                    "${InnerSurface}" \
+                    "${OuterSurface}" \
+                    -volume-roi "${WorkingDirectory}/${Prefix}_goodvoxels.nii.gz" \
+                    -dilate-missing ${dilation_dist} \
+                        -nearest
+
+        else
+
+            "${WB}" -volume-to-surface-mapping \
+                "${VolumefMRI}.nii.gz" \
+                "${MidSurface}" \
+                "${NativefMRI}" \
+                -ribbon-constrained \
+                    "${InnerSurface}" \
+                    "${OuterSurface}" \
+                    -dilate-missing ${dilation_dist} \
+                        -nearest
+
+        fi
+
+
+        "${WB}" -metric-dilate \
+            "${NativefMRI}" \
+            "${MidSurface}" \
+            "${dilation_dist}" \
+            "${NativefMRI}" \
+            -nearest
+
+        "${WB}" -metric-mask \
+            "${NativefMRI}" \
+            "${NativeROI}" \
+            "${NativefMRI}"
+
+        log_Msg "Generated fMRI file in native space at: ${NativefMRI}"
+
+
+        # =====================================================================
+        # Resample complete fMRI timeseries to all densities
+        # =====================================================================
+
+        for Mesh in ${Meshes[@]}; do
+
+            MeshFolder="${HippUnfoldFolder}/${Mesh}"
+
+            TargetFlat="${MeshFolder}/${Prefix}_flat.${Mesh}.surf.gii"
+            TargetMidSurface="${MeshFolder}/${Prefix}_midthickness.${Mesh}.surf.gii"
+            TargetROI="${WorkingDirectory}/${Prefix}_ones.${Mesh}.func.gii"
+
+            TargetfMRI="${WorkingDirectory}/${Prefix}_fMRI.${Mesh}.func.gii"
+
+
+            "${WB}" -metric-resample \
+                "${NativefMRI}" \
+                "${NativeFlat}" \
+                "${TargetFlat}" \
+                ADAP_BARY_AREA \
+                "${TargetfMRI}" \
+                -area-surfs \
+                    "${MidSurface}" \
+                    "${TargetMidSurface}" \
+                -current-roi "${NativeROI}" \
+                -bypass-sphere-check
+
+
+            "${WB}" -metric-dilate \
+                "${TargetfMRI}" \
+                "${TargetMidSurface}" \
+                ${dilation_dist} \
+                "${TargetfMRI}" \
+                -nearest
+
+
+            "${WB}" -metric-mask \
+                "${TargetfMRI}" \
+                "${TargetROI}" \
+                "${TargetfMRI}"
+
+
+            log_Msg "Generated fMRI file in ${Mesh} mesh at: ${TargetfMRI}"
+
+
+            done
+        done
+    done
+done
+
+# =====================================================================
+# All voxel mask an good voxel mask
+# =====================================================================
+
+fslmaths "${WorkingDirectory}/${Subject}.L.hipp.ribbon.nii.gz" \
+    -add "${WorkingDirectory}/${Subject}.R.hipp.ribbon.nii.gz" \
+    -add "${WorkingDirectory}/${Subject}.L.dentate.ribbon.nii.gz" \
+    -add "${WorkingDirectory}/${Subject}.R.dentate.ribbon.nii.gz" \
+    -bin \
+    "${WorkingDirectory}/${Subject}.allStructures.allVoxels.ribbon.nii.gz"
+
+fslmaths "${WorkingDirectory}/${Subject}.L.hipp_goodvoxels.nii.gz" \
+    -add "${WorkingDirectory}/${Subject}.R.hipp_goodvoxels.nii.gz" \
+    -add "${WorkingDirectory}/${Subject}.L.dentate_goodvoxels.nii.gz" \
+    -add "${WorkingDirectory}/${Subject}.R.dentate_goodvoxels.nii.gz" \
+    -bin \
+    "${WorkingDirectory}/${Subject}.all_structures.goodvoxels.nii.gz"
