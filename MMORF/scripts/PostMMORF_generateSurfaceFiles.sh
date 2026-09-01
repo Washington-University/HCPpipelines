9807d28ec2af4aec1356a295fd2f93bf89dd553f
Azure2003 <alexander.haoyang.zhou@gmail.com>
2026-09-01 14:07:26 -0500

    Got rid of the new opts guess directory things

--- MMORF/scripts/PostMMORF_generateSurfaceFiles.sh
+++ MMORF/scripts/PostMMORF_generateSurfaceFiles.sh
@@ -22,23 +22,6 @@ if [ "$#" = "0" ]; then
     exit 1
 fi
 
-# ------------------------------------------------------------------------------
-#  Check that HCPPIPEDIR is defined and Load Function Libraries
-# ------------------------------------------------------------------------------
-
-pipedirguessed=0
-if [[ "${HCPPIPEDIR:-}" == "" ]]
-then
-    pipedirguessed=1
-    #fix this if the script is more than one level below HCPPIPEDIR
-    export HCPPIPEDIR="$(dirname -- "$0")/../.."
-fi
-
-
-if [ -z "${HCPPIPEDIR}" ]; then
-  echo "${script_name}: ABORTING: HCPPIPEDIR environment variable must be set"
-  exit 1
-fi
 
 source "${HCPPIPEDIR}/global/scripts/debug.shlib" "$@"         # Debugging functions; also sources log.shlib
 source ${HCPPIPEDIR}/global/scripts/opts.shlib                 # Command line option functions
