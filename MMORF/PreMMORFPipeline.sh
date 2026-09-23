796514beae8b8b41a9f984814661856073cafcb4
Alex Zhou <94024008+Azure2003@users.noreply.github.com>
2026-09-22 20:03:04 -0500

    Update MMORF/PreMMORFPipeline.sh
    Co-authored-by: Tim Coalson <coalsont@users.noreply.github.com>

--- MMORF/PreMMORFPipeline.sh
+++ MMORF/PreMMORFPipeline.sh
@@ -49,8 +49,7 @@ T1wRestore=${T1wFolder}/${T1wImage}_acpc_dc_restore
 
 
 
-T1wRestoreBasename=`${FSLDIR}/bin/remove_ext $T1wRestore`;
-T1wRestoreBasename=`basename $T1wRestoreBasename`;
+T1wRestoreBasename=$(basename "$(remove_ext "$T1wRestore")")
 #T1wRestoreBrainBasename=`remove_ext $T1wRestoreBrain`;
 #T1wRestoreBrainBasename=`basename $T1wRestoreBrainBasename`;
 
